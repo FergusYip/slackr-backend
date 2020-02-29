3246a483942b70623c4946deba4139d4c8c6bae4
Cameron Day <cameronday@Camerons-MacBook-Pro.local>
2020-02-29 19:03:19 +1100

    Specifying the dictionary location of new users created in the tests as opposed to using the default 12345 token. This will allow for tests on completed versions of Slackr to use unique u_ids other than test cases.

--- src/message_test.py
+++ src/message_test.py
@@ -11,42 +11,71 @@ import auth
 
 # Simple tests of average case messages.
 def test_message_send():
-    auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    assert(channels.create('12345', 'Hello', True)
-    == {'channel_id' : 1})
-    assert(message.send('12345', 1, 'Hello world!')
-    == {'message_id' : 1})
-    assert(message.send('12345', 1, 'testingamessagenospaces')
-    == {'message_id' : 1})
-    assert(message.send('12345', 1, '12176182812')
-    == {'message_id' : 1})
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
+
+    channel_id = channels.create(new_user['token'], 'Channel1', True)
+
+    new_message = message.send(new_user['token'], channel_id['channel_id'], 'Hello world!')
+    messages = channel.messages(new_user['token'], channel_id['channel_id'], 0)
+
+    assert(new_message['message_id'] == messages[0]['message_id'])
+
+    second_message = message.send(new_user['token'], channel_id['channel_id'], 'testingamessagenospaces')
+    messages = channel.messages(new_user['token'], channel_id['channel_id'], 0)
+
+    assert(second_message['message_id'] == messages[0]['message_id'])
+
+    third_message = message.send(new_user['token'], channel_id['channel_id'], 'test message 123')
+    messages = channel.messages(new_user['token'], channel_id['channel_id'], 0)
+
+    assert(second_message['message_id'] == messages[1]['message_id'])
 
 # Testing unauthorized sending of messages.
 def test_unauthorized():
-    auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    assert(channels.create('12345', 'areyouallowedin', False)
-    == {'channel_id' : 1})
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
+
+    channel_id = channels.create(new_user['token'], 'Channel2', False)
+
     with pytest.raises(AccessError) as e:
-        message.send('12345', 1, 'Hello team')
+        message.send(new_user['token'], channel_id['channel_id'], 'Hello team')
 
 # Testing a change in authorization affecting message sending.
 def test_authorization_change():
-    auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    assert(channels.create('12345', 'WorkChannelTest', False)
-    == {'channel_id' : 1})
-    with pytest.raises(AccessError) as f:
-        message.send('12345', 1, 'Text Example')
-    channel.invite('12345', 1, 1)
-    assert(message.send('12345', 1, 'Hello World!') == { 'message_id' : 1})
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
+
+    channel_id = channels.create(new_user['token'], 'Channel3', False)
+
+    second_user = auth.register('letmein@test.com', 'Password1', 'Sample', 'Name')
+
+    with pytest.raises(AccessError) as e:
+        message.send(second_user['token'], channel_id['channel_id'], 'Why wont this send?')
+
+    channel.invite(new_user['token'], channel_id['channel_id'], second_user['u_id'])
+
+    new_message = message.send(second_user['token'], channel_id['channel_id'], 'Hello World!')
+    messages = channel.messages(second_user['token'], channel_id['channel_id'], 0)
+
+    assert(new_message['message_id'] == messages[0]['message_id'])
 
 # Testing an incorrect character length string message.
 def test_inputErrors():
-    auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    assert(channels.create('12345', 'NewChannel', True))
-    with pytest.raises(InputError) as g:
-        message.send('12345', 1, 'i' * 1001)
-    assert(message.send('12345', 1, 'Hello World!') == {'message_id' : 1})
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
+
+    channel_id = channels.create(new_user['token'], 'Channel4', True))
+
+    with pytest.raises(InputError) as e:
+        message.send(new_user['token'], channel_id['channel_id'], 'i' * 1001)
+
+    new_message = message.send(second_user['token'], channel_id['channel_id'], 'i' * 1000)
+    messages = channel.messages(new_user['token'], channel_id['channel_id'], 0)
+
+    assert(new_message['message_id'] == messages[0]['message_id'])
 
 # ========== TESTING MESSAGE REMOVE FUNCTION ==========
+
+def test_messageInputError():
+    pass
+
+
 
 # ========== TESTING MESSAGE EDIT FUNCTION ==========
