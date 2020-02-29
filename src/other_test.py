13d01d3835545d003b2491e96887c300e6e0e78a
Wai Lam Fergus Yip <z5248282@unsw.edu.au>
2020-02-29 22:27:37 +1100

    Added basic test for users_all

--- src/other_test.py
+++ src/other_test.py
@@ -1,2 +1,17 @@
+import auth
 import other
+import user
 import pytest
+
+
+def test_users_all_basic():
+    avery = auth.register('averylogrono@email.com',
+                          'averylogrono', 'Avery', 'Logrono')
+    user.profile_sethandle(avery['token'], 'averylogrono')
+
+    all_users = other.users_all(avery['token'])
+    assert all_users['users'][0]['u_id'] == avery['u_id']
+    assert all_users['users'][0]['email'] == 'user.name@email.com'
+    assert all_users['users'][0]['name_first'] == 'First'
+    assert all_users['users'][0]['name_last'] == 'Last'
+    assert all_users['users'][0]['handle_str'] == 'averylogrono'
