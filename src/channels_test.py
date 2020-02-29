408b191d36455a8842434dfbeb8973661dae7105
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-01 00:29:39 +1100

    Completed tests for channels_list

--- src/channels_test.py
+++ src/channels_test.py
@@ -1,4 +1,5 @@
 import auth
+import channel
 import channels
 import pytest
 from error import InputError
@@ -33,3 +34,23 @@ def test_listall():
 
     for channel in all_channels:
         assert channel['channel_id'] in channel_ids
+
+
+def test_list():
+    user = auth.register('user.name@email.com', 'password', 'First', 'Last')
+    joined_1 = channels.create(user['token'], 'One', True)
+    joined_2 = channels.create(user['token'], 'Two', True)
+    not_joined = channels.create(user['token'], 'Three', True)
+
+    channel.channel_join(user['token'], joined_1['channel_id'])
+    channel.channel_join(user['token'], joined_2['channel_id'])
+
+    joined_channel_ids = [joined_1['channel_id'], joined_2['channel_id']]
+
+    all_joined_channels = channels.list(user['token'])['channels']
+    assert isinstance(all_joined_channels, list) == True
+    assert len(all_joined_channels) == 2
+
+    for chan in all_joined_channels:
+        assert chan['channel_id'] in joined_channel_ids
+        assert chan['channel_id'] != not_joined['channel_id']
