e243032d914c2ae83d833be0d070794fa61ce0d2
Cameron Day <cameronday@Camerons-MacBook-Pro.local>
2020-02-29 17:20:04 +1100

    Adding a function to test input errors in the message.send function

--- src/message_test.py
+++ src/message_test.py
@@ -6,9 +6,11 @@ import message
 from error import AccessError
 import pytest
 import user
+import auth
 
 # Simple tests of average case messages.
 def test_message_send():
+    channels.create('12345', 'Hello')
     assert(message.send('12345', 6, 'Hello world!') == {'message_id' : 1})
     assert(message.send('22342', 1, 'testingamessagenospaces') == {'message_id' : 1})
     assert(message.send('32982', 37, '12176182812') == {'message_id' : 1})
@@ -21,9 +23,12 @@ def test_unauthorized():
         assert(message.send('32932', 1, 'Testing the error.'))
 
 def test_authorization_change():
-    user.profile('12345', 1)
+    assert(auth.register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name') == {'u_id' : 1, 'token' : '12345'})
     assert(channels.create('12345', 'WorkChannelTest', False) == {'channel_id' : 1})
     with pytest.raises(AccessError) as f:
         assert(message.send('12345', 1, 'Text Example'))
     channel.invite('12345', 1, 1)
     assert(message.send('12345', 1, 'Hello World!'))
+
+def test_inputErrors():
+    pass
