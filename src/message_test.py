9cea5cacb979ab5ac3ee2103a69a544201274d51
Cameron <z5264584@unsw.edu.au>
2020-02-29 16:43:29 +1100

    Adding AccessErrors for message_send function

--- src/message_test.py
+++ src/message_test.py
@@ -1,10 +1,18 @@
 # Testing the message.py file
 
+import channels
 import message
+from error import AccessError
+import pytest
 
 # Simple tests of average case messages.
 def test_message_send():
     assert(message.send('12345', 6, 'Hello world!') == {'message_id' : 1})
     assert(message.send('22342', 1, 'testingamessagenospaces') == {'message_id' : 1})
     assert(message.send('32982', 37, '12176182812') == {'message_id' : 1})
+    
+def test_unauthorized():    
+    assert(channels.create('12345', 'areyouallowedin', False) == {'channel_id' : 1})    
+    with pytest.raises(AccessError) as e:
+        assert(message.send('12345', 1, 'Hello team'))
     
