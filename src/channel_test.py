de2eaab8c3820263012aa57ec04b501b43e9ef1e
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-03-02 18:08:59 +1100

    fixed test_invite_access() function

--- src/channel_test.py
+++ src/channel_test.py
@@ -6,18 +6,23 @@ from error import InputError
 from error import AccessError
 
 # Making a dummy user with valid details.
-dummy_user1 = auth.register('something.else@domain.com', 'GreatPassword04', 'something', 'else')
-dummy_user2 = auth.register('dummy.user@domain.com', 'BetterPassword09', 'dummy', 'user')
+dummy_user1 = auth.register(
+    'something.else@domain.com', 'GreatPassword04', 'something', 'else')
+dummy_user2 = auth.register(
+    'dummy.user@domain.com', 'BetterPassword09', 'dummy', 'user')
 
-# creating channel and inviting a user. 
+# creating channels.
 c_id1 = channels.create(dummy_user1['token'], 'name1', True)
 c_id2 = channels.create(dummy_user2['token'], 'name2', True)
 
+
 def test_invite_channel():
 
-    # testing channel invite function to non-existent channel.
-    channel.invite(dummy_user1['token'], c_id1['channel_id'], dummy_user2['u_id'])
+    # testing channel invite function to valid channel.
+    channel.invite(dummy_user1['token'],
+                   c_id1['channel_id'], dummy_user2['u_id'])
 
+    # testing channel invite function to invalid channel.
     with pytest.raises(InputError) as e:
         channel.invite(dummy_user1['token'], '3555', dummy_user2['u_id'])
 
@@ -27,24 +32,21 @@ def test_invite_user():
     with pytest.raises(InputError) as e:
         channel.invite(dummy_user1['token'], c_id1['channel_id'], 69)
 
-    # testing if the user with u_id 1 exists in channel with channel id c_id1.
+    # testing if the user with user id dummy_user2[u_id] exists in channel c_id1.
+    # this should pass if line 19 executed.
     details = channel.details(dummy_user1['token'], c_id1['channel_id'])
     user_in_channel = False
 
     for user in details['all_members']:
-        if user['u_id'] == 1:
+        if user['u_id'] == dummy_user2['u_id']:
             user_in_channel = True
 
     assert user_in_channel == True
 
+
 def test_invite_access():
     # testing case when inviting user is not a member of a channel.
-    
-    channel.create(dummy_user2['token'], 'name2', True)
+    # at this point - the channel name1 has both users (dummy_user1 and dummy_user2)
+    # but the channel name2 only has dummy_user2.
     with pytest.raises(AccessError) as e:
         channel.invite(dummy_user1['token'], c_id2, dummy_user2['u_id'])
-
-    
-
-
-
