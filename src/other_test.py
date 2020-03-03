919062b5eb8beea3e9c64772f64f1e8d521193ea
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-04 09:44:44 +1100

    Added AccessError test to user_all

--- src/other_test.py
+++ src/other_test.py
@@ -1,15 +1,29 @@
+import pytest
 import auth
 import other
 import user
-import pytest
+from error import AccessError
+
+
+@pytest.fixture
+def invalid_token():
+    temp_user = auth.auth_register('test.user@email.com', 'password', 'First',
+                                   'Last')
+    assert auth.auth_logout(temp_user['token'])
+    return temp_user['token']
 
 
 def test_users_all_basic():
-    avery = auth.register('averylogrono@email.com',
-                          'averylogrono', 'Avery', 'Logrono')
-    user.profile_sethandle(avery['token'], 'averylogrono')
+    avery = auth.auth_register('averylogrono@email.com', 'averylogrono',
+                               'Avery', 'Logrono')
+    user.user_profile_sethandle(avery['token'], 'averylogrono')
 
-    avery_profile = user.profile(avery['token'], avery['u_id'])['user']
+    avery_profile = user.user_profile(avery['token'], avery['u_id'])['user']
     all_users = other.users_all(avery['token'])
 
     assert avery_profile in all_users['users']
+
+
+def test_users_all_invalid_token(invalid_token):
+    with pytest.raises(AccessError):
+        other.users_all(invalid_token)
