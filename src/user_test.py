2d9342777f25f518d6158130f5efc91a70b0b609
Cameron Day <cameronday@Camerons-MacBook-Pro.local>
2020-03-03 18:40:24 +1100

    Adding tests for the user_profile_setname function.

--- src/user_test.py
+++ src/user_test.py
@@ -60,3 +60,39 @@ def test_invalid_uid():
 # =====================================================
 # ====== TESTING USER PROFILE SETNAME FUNCTION ========
 # =====================================================
+
+def test_averagecase_setname():
+    # Creating a user and calling user_profile_setname without raising an error.
+    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    user.user_profile_setname(new_user['token'], 'Ipsum', 'Lorem')
+
+def test_firstzerocharacter():
+    # Creating a user and changing their first name to be 0 characters. Thus,
+    # raising an InputError as this is invalid.
+    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    with pytest.raises(InputError) as e:
+        user.user_profile_setname(new_user['token'], '', 'Lorem')
+
+def test_firstoverfifty():
+    # Creating a user and chaning thier first name to be over 50 characters.
+    # Thus, raising an InputError.
+    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    with pytest.raises(InputError) as e:
+        user.user_profile_setname(new_user['token'], 'i' * 51, 'Lorem')
+
+def test_lastzerocharacter():
+    # Creating a user and changing their last name to be 0 characters. Thus,
+    # raising an InputError.
+    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    with pytest.raises(InputError) as e:
+        user.user_profile_setname(new_user['token'], 'Ipsum', '')
+
+def test_lastoverfifty():
+    # Creating a user and changing their last name to be over 50 characters.
+    # Thus, raising an InputError.
+    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    with pytest.raises(InputError) as e:
+        user.user_profile_setname(new_user['token'], 'Ipsum', 'i' * 50)
+
+'''Possible testing of invalid token. I.e. if a second user tries to change
+the target user's name without being authorized. AccessError?'''
