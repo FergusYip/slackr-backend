f35f9fe4963537670e677d9efcebcda094b5b5a4
Cameron Day <cameronday@Camerons-MacBook-Pro.local>
2020-03-03 13:02:17 +1100

    Adding pytest functions to test the user_profile functionality. Specifically testing each dictionary item is what it should be, as well as an InputError test

---
+++ src/user_test.py
@@ -0,0 +1,39 @@
+import user
+import channel
+import channels
+import auth
+from error import AccessError, InputError
+
+# =====================================================
+# ========== TESTING MESSAGE SEND FUNCTION ============
+# =====================================================
+
+def test_averagecase_uid():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    profile_information = user.profile(new_user['token'], new_user['u_id'])
+    assert(new_user['u_id'] == profile_information['user']['u_id'])
+
+def test_averagecase_email():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    profile_information = user.profile(new_user['token'], new_user['u_id'])
+    assert('test@test.com' == profile_information['user']['email'])
+
+def test_averagecase_firstname():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    profile_information = user.profile(new_user['token'], new_user['u_id'])
+    assert('Lorem' == profile_information['user']['name_first'])
+
+def test_averagecase_lastname():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    profile_information = user.profile(new_user['token'], new_user['u_id'])
+    assert('Ipsum' == profile_information['user']['name_last'])
+
+def test_averagecase_handle():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    profile_information = user.profile(new_user['token'], new_user['u_id'])
+    assert('lipsum' == profile_information['user']['handle_str'])
+
+def test_inputError():
+    new_user = auth.register('test@test.com', 'PaSsWoRd1', 'Lorem', 'Ipsum')
+    with pytest.raises(InputError) as e:
+        user.profile(new_user['token'], 'NOTAUID')
