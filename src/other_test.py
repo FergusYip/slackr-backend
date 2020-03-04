95ea06fbfc3bd8b6f097da7ed86ec1a8210c44ff
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-04 17:50:26 +1100

    Added test for multiple user for user_all function

--- src/other_test.py
+++ src/other_test.py
@@ -8,12 +8,23 @@ import user
 from error import AccessError
 
 
-def test_users_all(test_user):
+def test_users_all_single_user(test_user):
     test_user_profile = user.user_profile(test_user['token'],
                                           test_user['u_id'])['user']
     all_users = other.users_all(test_user['token'])
 
     assert test_user_profile in all_users['users']
+
+
+def test_users_all_multiple_users(new_user):
+    user_1 = new_user('user_1@email.com')
+    assert len(other.users_all(user_1['token'])['users']) == 1
+
+    user_2 = new_user('user_2@email.com')
+    assert len(other.users_all(user_2['token'])['users']) == 2
+
+    user_3 = new_user('user_3@email.com')
+    assert len(other.users_all(user_3['token'])['users']) == 3
 
 
 def test_users_all_invalid_token(invalid_token):
