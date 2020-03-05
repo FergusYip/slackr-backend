2735549033ce233b16d9fdbd0d01178f4f390213
Cameron Day <cameronday@Camerons-MacBook-Pro.local>
2020-03-06 01:45:38 +1100

    Updating files in message_edit_test.py to include fixtures and general cleanup

--- src/message_test/message_edit_test.py
+++ src/message_test/message_edit_test.py
@@ -11,48 +11,42 @@ import auth
 # ========== TESTING MESSAGE EDIT FUNCTION ============
 # =====================================================
 
-def test_averageCaseEdit():
-    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    channel_id = channels.channels_create(new_user['token'], 'Channel12', True)
-    new_message = message.message_send(new_user['token'], channel_id['channel_id'], 'Your good at programming')
-    message.message_edit(new_user['token'], new_message['message_id'], "You're** Oops spelling")
+def test_averageCaseEdit(test_channel, test_user):
+    # Average case test where a user edits their own message.
+    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
+    message.message_edit(test_user['token'], new_message['message_id'], 'Message')
 
-def test_emptyStringDelete():
-    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    channel_id = channels.channels_create(new_user['token'], 'Channel13', True)
-    new_message = message.message_send(new_user['token'], channel_id['channel_id'], 'I dont like you anymore')
+def test_emptyStringDelete(test_channel, test_user):
+    # Testing the change from a string to an empty string. Should result in a deleted message.
+    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
     message.message_edit(new_user['token'], new_message['message_id'], "")
+    # Assert that the most recent
+    messages = channel.channel_messages(test_user['token'], test_channel['channel_id'], 0)
+    assert(new_message['message_id'] != messages[0]['message_id'])
 
-    messages = channel.channel_messages(new_user['token'], channel_id['channel_id'], 0) # Update the messages variable.
-    assert(new_message['message_id'] != messages[0]['message_id']) # Ensure the edited message is deleted and no longer the most recent message.
+def test_OwnerEdit(test_channel, test_user, new_user):
+    # Testing the ability for the channel owner to edit a default user's message.
+    second_user = new_user('tester2@gmail.com')
+    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
+    new_message = message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
+    message.message_edit(test_user['token'], new_message['message_id'], 'New Message')
 
-def test_OwnerEdit():
-    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    second_user = auth.auth_register('tester2@test.com', 'Password42', 'Lorem', 'Ipsum')
-    # Creating a private channel that second_user cannot access.
-    channel_id = channels.channels_create(new_user['token'], 'Channel16', True)
-    channel.channel_invite(new_user['token'], channel_id['channel_id'], second_user['u_id'])
-    new_message = message.message_send(second_user['token'], channel_id['channel_id'], 'edit me')
-    message.message_edit(new_user['token'], new_message['message_id', "hello there"])
+def test_AccessErrorUnauthorized(test_channel, test_user, new_user):
+    # Testing an AccessError thrown when a default user tries to edit an owner's message.
+    second_user = new_user('tester2@gmail.com')
+    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
+    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
+    # Should raise an AccessError where a default user attempts to edit an owner's message.
+    with pytest.raises(AccessError):
+        message.message_edit(second_user['token'], new_message['message_id'], "New Message")
 
-def test_AccessErrorUnauthorized():
-    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    second_user = auth.auth_register('anotheremail@gmail.com', 'Password', 'Lorem', 'Ipsum')
-    channel_id = channels.channels_create(new_user['token'], 'Channel15', True)
-    channel.channel_invite(new_user['token'], channel_id['channel_id'], second_user['u_id'])
-    new_message = message.message_send(new_user['token'], channel_id['channel_id'], 'Hola el mundo!')
-
-    with pytest.raises(AccessError) as e:
-        message.message_edit(second_user['token'], new_message['message_id'], "el ladron!")
-
-def test_AccessErrorNotOwner():
-    new_user = auth.auth_register('test@test.com', 'PaSsWoRd1', 'Dummy', 'Name')
-    second_user = auth.auth_register('anotheremail@gmail.com', 'Password', 'Lorem', 'Ipsum')
-    third_user = auth.auth_register('wow@gmail.com', 'Password', 'CreativeName', 'Generator')
-
-    channel_id = channels.channels_create(new_user['token'], 'Channel15', True)
-    channel.channel_invite(new_user['token'], channel_id['channel_id'], second_user['u_id'])
-    channel.channel_invite(new_user['token'], channel_id['channel_id'], third_user['u_id'])
-    new_message = message.message_send(second_user['token'], channel_id['channel_id'], 'Bienvenidos!')
-    with pytest.raises(AccessError) as e:
-        message.message_edit(third_user['token'], new_message['message_id'], "el ladron!")
+def test_AccessErrorNotOwner(test_channel, test_user, new_user):
+    # Testing an AccessError thrown when a default user attempts to edit another default user's message.
+    second_user = new_user('tester2@gmail.com')
+    third_user = new_user('tester3@gmail.com')
+    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
+    channel.channel_invite(test_user['token'], test_channel['channel_id'], third_user['u_id'])
+    new_message = message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
+    # Should raise an AccessError when a default user attempts to edit another default user's message.
+    with pytest.raises(AccessError):
+        message.message_edit(third_user['token'], new_message['message_id'], "New Message")
