875ebee0e4f05e4b793508a516d3a4335f1ae70c
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-15 20:53:10 +1100

    Added permissions field to data store

--- src/data_store.py
+++ src/data_store.py
@@ -1,4 +1,11 @@
-data_store = {'users': [], 'channels': [], 'tokens': []}
+PERMISSIONS = {'owner': 1, 'member': 2}
+
+data_store = {
+    'users': [],
+    'channels': [],
+    'tokens': [],
+    'permissions': PERMISSIONS
+}
 
 if __name__ == "__main__":
     pass
