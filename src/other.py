043c808ef1f498b1687f760725d9d6a7edea3c82
Wai Lam Fergus Yip <z5248282@unsw.edu.au>
2020-03-15 16:36:31 +1100

    Completed search

--- src/other.py
+++ src/other.py
@@ -1,9 +1,9 @@
 import sys
-import jwt
 from json import dumps
+import jwt
 from flask import Flask, request
 from flask_cors import CORS
-from error import AccessError, InputError
+from error import AccessError
 
 APP = Flask(__name__)
 CORS(APP)
@@ -14,6 +14,13 @@ SECRET = 'the chunts'
 
 data_store = {'users': [], 'channels': [], 'tokens': []}
 
+def user_channels(u_id):
+    '''Retrieve a list of a user's joined channels'''
+    return [channel for channel in data_store['channels'] if u_id in channel['all_members']]
+
+def channel_search(channel, query_str):
+    '''Retrieve all messages in a channel which contain the query string'''
+    return [message for message in channel['messages'] if query_str in message['message']]
 
 @APP.route("/users/all", methods=['GET'])
 def users_all():
@@ -38,7 +45,21 @@ def users_all():
 
 @APP.route("/search", methods=['GET'])
 def search():
-    pass
+    token = request.args.get('token')
+    query_str = request.args.get('query_str')
+
+    try:
+        payload = jwt.decode(token.encode('utf-8'), SECRET)
+    except:
+        raise AccessError(description='Invalid token')
+
+    messages = []
+    for channel in user_channels(payload['u_id']):
+        channel_results = channel_search(channel, query_str)
+        messages.append(channel_results)
+
+    return dumps({'messages': messages})
+
 
 if __name__ == "__main__":
     APP.run(debug=True,
