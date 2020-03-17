3b15cd320ceaa75e7d24ca4292aa50a8b96d3129
Cameron Day <z5264584@unsw.edu.au>
2020-03-17 16:32:59 +1100

    Completing the message_edit function with error catching and updated json payload information.

--- src/message.py
+++ src/message.py
@@ -16,7 +16,7 @@ CORS(APP)
 
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 
-message = Blueprint('message', __name__)
+MESSAGE = Blueprint('message', __name__)
 
 def utc_now():
     return int(datetime.now(timezone.utc).timestamp())
@@ -33,7 +33,7 @@ def generate_message_id(channel_id):
         messageID = 1
     else:
         messageIDs = [message['message_id'] for message in channel_info['messages']]
-        messageID = max(messageIDs) + 1 
+        messageID = max(messageIDs) + 1
 
 def get_message(message_id):
     for message in data_store['channels']['messages']:
@@ -64,7 +64,7 @@ def is_user_admin(u_id, channel_id):
         return True
     return False
 
-@message.route("/send", methods=['POST'])
+@MESSAGE.route("/send", methods=['POST'])
 def message_send():
     channelid = request.args.get('channel_id')
     channel_info = get_channel(channelid)
@@ -77,10 +77,10 @@ def message_send():
     message = request.args.get('message')
     time_now = utc_now()
 
-    if (len(message) > 1000):
+    if len(message) > 1000:
         raise InputError(
             description='Message is greater than 1,000 characters')
-    
+
     if userID not in channel_info['all_members']:
         raise AccessError(
             description='User does not have Access to send messages in the current channel')
@@ -93,40 +93,51 @@ def message_send():
         'reacts': [],
         'is_pinned': False
     }
-    
+
     data_store['channels']['messages'].append(message)
 
     return dumps({
         'message_id': messageID
     })
 
-
-@message.route("/remove", methods=['DELETE'])
-def message_remove(token, message_id):
+@MESSAGE.route("/remove", methods=['DELETE'])
+def message_remove():
     token = request.args.get('token')
     payload = decode_token(token)
     userID = payload['u_id']
 
+    message_info = get_message(message_id)
+
     channelid = request.args.get('channel_id')
-    
+    channel_info = get_channel(channelid)
+
     if not message_existance(message_id):
         raise InputError(
             description='Message does not exist')
-    
-    message_info = get_message(message_id)
 
     if message_info['u_id'] != userID and not is_user_admin(userID, channelid):
         raise AccessError(
             description='User does not have access to remove this message')
-    
-    channel_info = get_channel(channelid)
 
     channel_info['messages'].remove(message_info)
 
     return dumps({})
 
+@MESSAGE.route("/edit", methods=['PUT'])
 def message_edit(token, message_id, message):
-    return {
+    payload = request.get_json()
+    token = payload['token']
+    user_id = payload['u_id']
+
+    message_id = payload['message_id']
+    new_message = payload['message']
+
+    channelid = payload['channel_id']
+    channel_info = get_channel(channelid)
+
+
+
+
     }
 
 def message_sendlater(token, channel_id, message, time_sent):
@@ -151,6 +162,5 @@ def message_unpin(token, message_id):
     }
 
 if __name__ == "__main__":
-    print('hello?')
     APP.run(debug=True,
             port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
