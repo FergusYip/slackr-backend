6ba73899ebcf54053b2be41c379702ccd5e1efc1
Cameron Day <z5264584@unsw.edu.au>
2020-03-17 12:52:55 +1100

    Completing the draft for the message/send functionality. Tidying up the code for improved readability, and performing syntax checks.

--- src/message.py
+++ src/message.py
@@ -7,8 +7,8 @@ from flask import Flask, request, Blueprint
 from flask_cors import CORS
 from error import AccessError, InputError
 from email_validation import invalid_email
-from datetime import datetime, timedelta
-from data_store import data_store, PERMISSIONS, SECRET, OWNER, MEMBER
+from datetime import datetime, timedelta, timezone
+from data_store import data_store, SECRET, OWNER, MEMBER
 from token_validation import decode_token
 
 APP = Flask(__name__)
@@ -16,45 +16,60 @@ CORS(APP)
 
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 
-@APP.route("/send", methods=['POST'])
-def message_send(token, channel_id, message):
-    token = request.args.get('token')
-    channelid = request.args.get('channel_id')
-    message = request.args.get('message')
+message = Blueprint('message', __name__)
 
+def utc_now():
+    return int(datetime.now(timezone.utc).timestamp())
+
+def get_channel(channel_id):
+    for channel in data_store['channels']:
+        if channel_id == channel['channel_id']:
+            return channel
+    return None
+
+def generate_message_id(channel_id):
+    channel_info = get_channel(channel_id)
+    if not channel_info['messages']:
+        messageID = 1
+    else:
+        messageIDs = [message['message_id'] for message in channel_info['messages']]
+        messageID = max(messageIDs) + 1 
+
+@message.route("/send", methods=['POST'])
+def message_send():
+    channelid = request.args.get('channel_id')
+    channel_info = get_channel(channelid)
+    messageID = generate_message_id(channelid)
+
+    token = request.args.get('token')
     payload = decode_token(token)
     userID = payload['u_id']
+
+    message = request.args.get('message')
+    time_now = utc_now()
 
     if (len(message) > 1000):
         raise InputError(
             description='Message is greater than 1,000 characters')
     
-    for channels in data_store['channels']:
-        if channelid == channels['channel_id']:
-            if userID not in channels['all_members']:
-                raise AccessError(
-                    description='User does not have Access to send messages in the current channel')
-            break
-
-    for channels in data_store['channels']:
-        if channelid == channels['channel_id']:
-            if not channels['messages']:
-                messageID = 1
-            else:
-                messageIDs = [message['message_id'] for message in channels['messages']]
-                messageID = max(messageIDs) + 1 
+    if userID not in channel_info['all_members']:
+        raise AccessError(
+            description='User does not have Access to send messages in the current channel')
 
     message = {
-        'message_id': messageID
-        'u_id': userID
-        'message': message
-        'time_created':
-        'reacts':
+        'message_id': messageID,
+        'u_id': userID,
+        'message': message,
+        'time_created': time_now,
+        'reacts': [],
+        'is_pinned': False
     }
+    
+    data_store['channels']['messages'].append(message)
 
-    return {
-        'message_id': 1,
-    }
+    return dumps({
+        'message_id': messageID
+    })
 
 def message_remove(token, message_id):
     return {
@@ -84,3 +99,8 @@ def message_pin(token, message_id):
 def message_unpin(token, message_id):
     return {
     }
+
+if __name__ == "__main__":
+    print('hello?')
+    APP.run(debug=True,
+            port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
