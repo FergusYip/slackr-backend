54cc3625e30e38b11a17d80db25a8af61997b0bc
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-03-17 15:52:27 +1100

    added implementation for channel_invite

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,50 @@
-def channel_invite(token, channel_id, u_id):
-    return {
-    }
+import sys
+import jwt
+from json import dumps
+from flask import Flask, request, Blueprint
+from flask_cors import CORS
+from error import AccessError, InputError
+from data_store import data_store, SECRET
+from token_validation import decode_token
+
+APP = Flask(__name__)
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+
+channel = Blueprint('channel', __name__)
+
+
+@channel.route("/invite", methods=['POST'])
+def channel_invite():
+    payload = request.get_json()
+
+    token = payload['token']
+    c_id = payload['channel_id']
+    invited = payload['u_id']
+
+    token_data = decode_token(token)
+
+    if invited not in data_store['users']:
+        raise InputError(description='User does not exist.')
+
+    add_into_channel(token_data['u_id'], c_id, invited)
+
+    return dumps({})
+
+
+def add_into_channel(inviter, c_id, invited):
+    '''
+    Appends a user ID into the channel with ID c_id.
+    '''
+    for channels in data_store['channels']:
+        if channels['channel_id'] == c_id:
+            if inviter not in channels['all_members']:
+                raise AccessError(
+                    description='User does not have permission to invite')
+            else:
+                channels['all_members'].append(invited)
+
 
 def channel_details(token, channel_id):
     return {
@@ -21,6 +65,7 @@ def channel_details(token, channel_id):
         ],
     }
 
+
 def channel_messages(token, channel_id, start):
     return {
         'messages': [
@@ -35,17 +80,21 @@ def channel_messages(token, channel_id, start):
         'end': 50,
     }
 
+
 def channel_leave(token, channel_id):
     return {
     }
+
 
 def channel_join(token, channel_id):
     return {
     }
 
+
 def channel_addowner(token, channel_id, u_id):
     return {
     }
+
 
 def channel_removeowner(token, channel_id, u_id):
     return {
