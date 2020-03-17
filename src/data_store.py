6870535c9e01b7368a7e535ef4c090950f29be50
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-17 11:53:03 +1100

    Added structure example as guide

--- src/data_store.py
+++ src/data_store.py
@@ -23,6 +23,42 @@ EMPTY_DATA_STORE = {
         'member': MEMBER
     }
 }
+'''
+Sample Data Store Structure
+
+data_store = {
+    'users': [{
+            'u_id': u_id,
+            'email': email,
+            'password': hash_pw(password),
+            'name_first': name_first,
+            'name_last': name_last,
+            'handle_str': generate_handle(name_first, name_last),
+            'permission_id': permission_id
+        }],
+    'channels': [{
+        'channel_id': channel_id,
+        'name': name,
+        'is_public': is_public,
+        'owner_members': [u_id],
+        'all_members': [u_id],
+        'messages': [{
+            'message_id': message_id,
+            'u_id': u_id,
+            'message': message,
+            'time_created': time_created,
+            'reacts': reacts, <- I assume this is a list
+            'is_pinned': is_pinned
+        }]
+    }],
+    'tokens': [],
+    'permissions': {
+        'owner': OWNER,
+        'member': MEMBER
+    }
+}
+
+'''
 
 if __name__ == "__main__":
     pass
