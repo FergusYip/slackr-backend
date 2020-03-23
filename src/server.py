75414820c1672ad4e4e18d7576ecbca0f6ed0631
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-23 13:48:55 +1100

    Removed routes

--- src/server.py
+++ src/server.py
@@ -56,90 +56,6 @@ def data():
     return dumps(data_store)
 
 
-# Example
-@APP.route("/echo", methods=['GET'])
-def echo():
-    data = request.args.get('data')
-    if data == 'echo':
-        raise InputError(description='Cannot echo "echo"')
-    return dumps({'data': data})
-
-
-@APP.route("/channel/invite", methods=['POST'])
-def channel_invite():
-    pass
-
-
-@APP.route("/channel/details", methods=['GET'])
-def channel_details():
-    pass
-
-
-@APP.route("/channel/messages", methods=['GET'])
-def channel_messages():
-    pass
-
-
-@APP.route("/channel/leave", methods=['POST'])
-def channel_leave():
-    pass
-
-
-@APP.route("/channel/join", methods=['POST'])
-def channel_join():
-    pass
-
-
-@APP.route("/channel/addowner", methods=['POST'])
-def channel_addowner():
-    pass
-
-
-@APP.route("/channel/removeowner", methods=['POST'])
-def channel_removeowner():
-    pass
-
-
-@APP.route("/message/send", methods=['POST'])
-def message_send():
-    pass
-
-
-@APP.route("/message/sendlater", methods=['POST'])
-def message_sendlater():
-    pass
-
-
-@APP.route("/message/react", methods=['POST'])
-def message_react():
-    pass
-
-
-@APP.route("/message/unreact", methods=['POST'])
-def message_unreact():
-    pass
-
-
-@APP.route("/message/pin", methods=['POST'])
-def message_pin():
-    pass
-
-
-@APP.route("/message/unpin", methods=['POST'])
-def message_unpin():
-    pass
-
-
-@APP.route("/message/remove", methods=['DELETE'])
-def message_remove():
-    pass
-
-
-@APP.route("/message/edit", methods=['PUT'])
-def message_edit():
-    pass
-
-
 @APP.route("/user/profile", methods=['GET'])
 def user_profile():
     pass
@@ -172,11 +88,6 @@ def standup_active():
 
 @APP.route("/standup/send", methods=['POST'])
 def standup_send():
-    pass
-
-
-@APP.route("/workspace/reset", methods=['POST'])
-def workspace_reset():
     pass
 
 
