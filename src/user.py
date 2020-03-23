a6b6dc63e11d64c11be1396513952ae2855da9da
Cameron Day <z5264584@unsw.edu.au>
2020-03-23 20:31:49 +1100

    FInishing the user.py functions with up-to-date data structure changing functions. Additionally added several helper functions to assist with changing data and error checking.

--- src/user.py
+++ src/user.py
@@ -1,13 +1,10 @@
 import sys
 import jwt
-import math
-import hashlib
 from json import dumps
 from flask import Flask, request, Blueprint
 from flask_cors import CORS
 from error import AccessError, InputError
 from email_validation import invalid_email
-from datetime import datetime, timedelta, timezone
 from data_store import data_store, SECRET
 from token_validation import decode_token
 import helpers
@@ -20,28 +17,114 @@ APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 USER = Blueprint('user', __name__)
 
 @USER.route('/profile', methods='GET')
-def user_profile(token, u_id):
-    return {
-        'user': {
-        	'u_id': 1,
-        	'email': 'cs1531@cse.unsw.edu.au',
-        	'name_first': 'Hayden',
-        	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
-        },
-    }
+def user_profile():
+
+    '''
+    Function that will return the profile information of a desired
+    user on the Slackr platform.
+    '''
+
+    payload = request.get_json()
+
+    token = payload['token']
+    token_info = decode_token(token)
+    user_id = token_info['u_id']
+
+    target_user = u_id
+
+    if not get_user(target_user):
+        raise InputError(
+            description='User ID is not a valid user')
+    
+    user_info = get_user(target_user)
+    
+    return dumps(user_info)
 
 @USER.route('/profile/setname', methods='PUT')
-def user_profile_setname(token, name_first, name_last):
-    return {
-    }
+def user_profile_setname():
+
+    '''
+    Function that will take a desired first and last name and will change
+    the authorized user's information to be updated with this information.
+    '''
+
+    payload = request.get_json()
+
+    token = payload['token']
+    token_info = decode_token(token)
+    user_id = token_info['u_id']
+
+    first_name = name_first
+    last_name = name_last 
+
+    if not helpers.check_name(first_name):
+        raise InputError(
+            description='First name is not between 1 and 50 characters')
+    
+    if not helpers.check_name(last_name):
+        raise InputError(
+            description='Last name is not between 1 and 50 characters')
+
+    helpers.user_change_first_last_name(user_id, first_name, last_name)
+
+    return dumps({})
 
 @USER.route('/profile/setemail', methods='PUT')
 def user_profile_setemail(token, email):
-    return {
-    }
+
+    '''
+    Function that will take a desired email and will change
+    the authorized user's information to be updated with this information.
+    '''
+
+    payload = request.get_json()
+
+    token = payload['token']
+    token_info = decode_token(token)
+    user_id = token_info['u_id']
+
+    desired_email = email
+
+    if invalid_email(desired_email):
+        raise InputError(
+            description='Email address is invalid')
+
+    if helpers.is_email_used(desired_email):
+        raise InputError(
+            description='Email address is already being used by another user')
+
+    helpers.user_change_email(user_id, desired_email)
+
+    return dumps({})
 
 @USER.route('/profile/sethandle', methods='PUT')
 def user_profile_sethandle(token, handle_str):
-    return {
-    }
+    
+    '''
+    Function that will take a desired handle and will change the authorized
+    user's information to reflect this new handle.
+    '''
+
+    payload = request.get_json()
+
+    token = payload['token']
+    token_info = decode_token(token)
+    user_id = token_info['u_id']
+
+    desired_handle = handle_str
+
+    if not helpers.handle_length_check(desired_handle):
+        raise InputError(
+            description='Handle is not between 2 and 20 characters')
+
+    if helpers.is_handle_used(desired_handle):
+        raise InputError(
+            description='Handle is already being used by another user')
+
+    helpers.user_change_handle(user_id, desired_handle)
+
+    return dumps({})
+
+if __name__ == "__main__":
+    APP.run(debug=True,
+            port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
