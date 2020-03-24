41f9a3903d4f40df0757bc34fce02cb7e2fda668
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-24 18:02:09 +1100

    Split routes and functions

--- src/other.py
+++ src/other.py
@@ -1,16 +1,51 @@
 import sys
 from json import dumps
-from flask import Flask, request, Blueprint
-from flask_cors import CORS
+from flask import request, Blueprint
 from data_store import data_store
 from token_validation import decode_token
 
-APP = Flask(__name__)
-CORS(APP)
-
-APP.config['TRAP_HTTP_EXCEPTIONS'] = True
-
 OTHER = Blueprint('other', __name__)
+
+
+@OTHER.route("/users/all", methods=['GET'])
+def route_users_all():
+    token = request.values.get('token')
+    return dumps(users_all(token))
+
+
+@OTHER.route("/search", methods=['GET'])
+def route_search():
+    token = request.values.get('token')
+    query_str = request.values.get('query_str')
+    return dumps(search(token, query_str))
+
+
+def users_all(token):
+    decode_token(token)
+
+    users = []
+    for user in data_store['users']:
+        user_dict = {
+            'u_id': user['u_id'],
+            'email': user['email'],
+            'name_first': user['name_first'],
+            'name_last': user['name_last'],
+            'handle_str': user['handle_str'],
+        }
+        users.append(user_dict)
+
+    return {'users': users}
+
+
+def search(token, query_str):
+    token_payload = decode_token(token)
+
+    messages = []
+    for channel in user_channels(token_payload['u_id']):
+        channel_results = channel_search(channel, query_str)
+        messages.append(channel_results)
+
+    return {'messages': messages}
 
 
 def user_channels(u_id):
@@ -29,40 +64,5 @@ def channel_search(channel, query_str):
     ]
 
 
-@OTHER.route("/users/all", methods=['GET'])
-def users_all():
-    token = request.values.get('token')
-    decode_token(token)
-
-    users = []
-    for user in data_store['users']:
-        user_dict = {
-            'u_id': user['u_id'],
-            'email': user['email'],
-            'name_first': user['name_first'],
-            'name_last': user['name_last'],
-            'handle_str': user['handle_str'],
-        }
-        users.append(user_dict)
-
-    return dumps({'users': users})
-
-
-@OTHER.route("/search", methods=['GET'])
-def search():
-    token = request.values.get('token')
-    query_str = request.values.get('query_str')
-
-    token_payload = decode_token(token)
-
-    messages = []
-    for channel in user_channels(token_payload['u_id']):
-        channel_results = channel_search(channel, query_str)
-        messages.append(channel_results)
-
-    return dumps({'messages': messages})
-
-
 if __name__ == "__main__":
-    APP.run(debug=True,
-            port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
+    pass
