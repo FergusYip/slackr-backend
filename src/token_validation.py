60649859c48b496fe54b90892c27ded9f4424cb8
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-24 13:32:36 +1100

    Changed exp to iat

--- src/token_validation.py
+++ src/token_validation.py
@@ -28,7 +28,7 @@ def decode_token(token):
     except:
         raise AccessError(description='Token is invalid')
 
-    if payload['exp'] > data_store['time_created']:
+    if payload['iat'] < data_store['time_created']:
         raise AccessError(description='Session has expired')
 
     if payload['u_id'] not in get_all_u_id():
