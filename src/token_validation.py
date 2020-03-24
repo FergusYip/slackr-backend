5d1af09eb3ae99f5345d4e57d560448178744314
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-24 13:41:25 +1100

    Fixed token error

--- src/token_validation.py
+++ src/token_validation.py
@@ -28,7 +28,7 @@ def decode_token(token):
     except:
         raise AccessError(description='Token is invalid')
 
-    if payload['iat'] < data_store['time_created']:
+    if payload['iat'] < int(data_store['time_created'].timestamp()):
         raise AccessError(description='Session has expired')
 
     if payload['u_id'] not in get_all_u_id():
