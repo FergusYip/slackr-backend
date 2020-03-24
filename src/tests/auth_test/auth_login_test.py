f68936cc075aacb572808e9490a7994c01c63e53
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-24 22:59:36 +1100

    Restored integration tests

--- src/tests/auth_test/auth_login_test.py
+++ src/tests/auth_test/auth_login_test.py
@@ -1,87 +1,56 @@
-import requests
 import pytest
+import auth
+from error import InputError
 
-BASE_URL = 'http://127.0.0.1:8080'
+
+@pytest.fixture
+def paris():
+    '''Fixture for a creating a user named Paris Cler'''
+
+    return auth.auth_register('pariscler@email.com', 'pariscler0229', 'Paris',
+                              'Cler')
 
 
-def test_login_return_type(new_user, reset):
+def test_login_return_type(paris):
     '''Test the types of values returned by auth_login'''
 
-    user_info = {
-        'email': 'test@email.com',
-        'password': 'password',
-    }
-
-    new_user(email=user_info['email'], password=user_info['password'])
-
-    user = requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
-
-    assert isinstance(user['u_id'], int)
-    assert isinstance(user['token'], str)
+    paris_login = auth.auth_login('pariscler@email.com', 'pariscler0229')
+    assert isinstance(paris_login['u_id'], int)
+    assert isinstance(paris_login['token'], str)
 
 
-def test_login_u_id(new_user, reset):
+def test_login_u_id(paris):
     '''Test that the u_id returned auth_login matches u_id returned by auth_register'''
 
-    user_info = {
-        'email': 'test@email.com',
-        'password': 'password',
-    }
-
-    register = new_user(email=user_info['email'],
-                        password=user_info['password'])
-
-    login = requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
-
-    assert register['u_id'] == login['u_id']
+    paris_login = auth.auth_login('pariscler@email.com', 'pariscler0229')
+    assert paris_login['u_id'] == paris['u_id']
 
 
-def test_login_password(new_user, reset):
+def test_login_password(paris):
     '''Test auth_login with an incorrect password'''
 
-    user_info = {
-        'email': 'test@email.com',
-        'password': 'incorrect password',  # This is not the correct password
-    }
-
-    new_user(email=user_info['email'], password='correct password')
-
-    error = requests.post(f"{BASE_URL}/auth/login", json=user_info)
-
-    with pytest.raises(requests.HTTPError):
-        requests.Response.raise_for_status(error)
+    # Password is not correct
+    with pytest.raises(InputError):
+        auth.auth_login('pariscler@email.com', 'incorrect_password')
 
 
-def test_login_invalid_user(reset):
+def test_login_invalid_user():
     '''Test with an email which does not belong to a user'''
 
-    user_info = {
-        'email': 'test@email.com',
-        'password': 'password',
-    }
-
-    error = requests.post(f"{BASE_URL}/auth/login", json=user_info)
-
-    with pytest.raises(requests.HTTPError):
-        requests.Response.raise_for_status(error)
+    with pytest.raises(InputError):
+        auth.auth_login('non_existent_user@email.com', '12345678')
 
 
-def test_login_multiple_sessions(new_user):
+def test_login_multiple_sessions(paris):
     '''Test that auth_login can create multiple session at once'''
 
-    user_info = {
-        'email': 'test@email.com',
-        'password': 'password',
-    }
-
-    new_user(email=user_info['email'], password=user_info['password'])
-
-    session_1 = requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
-    session_2 = requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
-    session_3 = requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
+    session_1 = auth.auth_login('pariscler@email.com', 'pariscler0229')
+    session_2 = auth.auth_login('pariscler@email.com', 'pariscler0229')
+    session_3 = auth.auth_login('pariscler@email.com', 'pariscler0229')
 
     session_tokens = [
-        session_1['token'], session_2['token'], session_3['token']
+        paris['token'], session_1['token'], session_2['token'],
+        session_3['token']
     ]
 
     # Verify that all session_tokens are unique
@@ -91,26 +60,13 @@ def test_login_multiple_sessions(new_user):
 def test_login_email_valid(valid_emails):
     '''Test input of valid emails into auth_login'''
 
-    user_info = {
-        'email': None,
-        'password': 'password',
-    }
-
     for email in valid_emails:
-        user_info['email'] = email
-        requests.post(f"{BASE_URL}/auth/login", json=user_info).json()
+        auth.auth_login(email, 'password')
 
 
 def test_login_email_invalid(invalid_emails):
     '''Test input of invalid emails into auth_login'''
 
-    user_info = {
-        'email': None,
-        'password': 'password',
-    }
-
     for email in invalid_emails:
-        user_info['email'] = email
-        error = requests.post(f"{BASE_URL}/auth/login", json=user_info)
-        with pytest.raises(requests.HTTPError):
-            requests.Response.raise_for_status(error)
+        with pytest.raises(InputError):
+            auth.auth_login(email, 'password')
--- src/tests/auth_test/auth_logout_test.py
+++ src/tests/auth_test/auth_logout_test.py
@@ -1,22 +1,24 @@
-import requests
 import pytest
+import auth
+from error import InputError, AccessError
 
-BASE_URL = 'http://127.0.0.1:8080'
+
+@pytest.fixture
+def paris():
+    '''Fixture for a creating a user named Paris Cler'''
+
+    return auth.auth_register('pariscler@email.com', 'pariscler0229', 'Paris',
+                              'Cler')
 
 
-def test_logout(reset, new_user):
+def test_logout(paris):
     '''Test that auth_logout returns True on successful Logout'''
 
-    user = new_user()
-    logout_input = {'token': user['token']}
-    logout = requests.post(f"{BASE_URL}/auth/logout", json=logout_input).json()
-    assert logout['is_success']
+    assert auth.auth_logout(paris['token'])['is_success']
 
 
-def test_logout_invalid_token(reset, invalid_token):
+def test_logout_invalid_token(invalid_token):
     '''Test that auth_logout raises an AccessError when given invalid token'''
 
-    logout_input = {'token': invalid_token}
-    error = requests.post(f"{BASE_URL}/auth/logout", json=logout_input)
-    with pytest.raises(requests.HTTPError):
-        requests.Response.raise_for_status(error)
+    with pytest.raises(AccessError):
+        auth.auth_logout(invalid_token)['is_success']
