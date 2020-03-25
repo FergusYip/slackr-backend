35b0c68a56433d9764b51a18e57e8c4aaa01e683
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-03-25 17:27:12 +1100

    completed standup start/stop

--- src/standup.py
+++ src/standup.py
@@ -1,7 +1,11 @@
 from json import dumps
+import threading
 from flask import request, Blueprint
 from token_validation import decode_token
 import helpers
+from error import AccessError, InputError
+from datetime import datetime, timezone
+from data_store import data_store
 
 STANDUP = Blueprint('standup', __name__)
 
@@ -33,13 +37,48 @@ def route_standup_send():
 
 
 def standup_start(token, channel_id, length):
-    pass
+    '''
+    Implementation of standup start function.
+    '''
+    decode_token(token)
+
+    channel = helpers.get_channel(channel_id)
+    if channel is None:
+        raise InputError(description='Channel does not exist.')
+
+    channel['standup']['is_active'] = True
+
+    timer = threading.Timer(length, stop_standup(channel_id))
+    timer.start()
+
+    finish = int(datetime.now(timezone.utc).timestamp()) + length
+
+    return finish
+
+
+def stop_standup(channel_id):
+    for channel in data_store['channels']:
+        if channel['channel_id'] == channel_id:
+            channel['standup']['is_active'] = False
+            correct_channel = channel
+
+    joined_message = ''
+
+    # for every message dictionary in messages[]
+    for message in correct_channel['standup']['messages']:
+        joined_message += f"{message['handle_str']}: {message['message']}\n"
 
 
 def standup_active(token, channel_id):
-    pass
+    # input error if channel does not exist.
+    channel = helpers.get_channel(channel_id)
+    if channel is None:
+        raise InputError(description='Channel does not exist.')
 
 
 def standup_send(token, channel_id, message):
+    # input error if channel does not exist.
+    channel = helpers.get_channel(channel_id)
+    if channel is None:
+        raise InputError(description='Channel does not exist.')
     return {}
-
