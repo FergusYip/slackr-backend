0afe5411d8802c627e54f375bf11563521e7a39d
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-25 18:31:42 +1100

    Fixed buggy code (NOT FULLY TESTED)

--- src/standup.py
+++ src/standup.py
@@ -5,31 +5,29 @@ from token_validation import decode_token
 from error import AccessError, InputError
 from data_store import data_store
 import helpers
-from error import AccessError, InputError
 from datetime import datetime, timezone
-from data_store import data_store
+from message import message_send
 
 STANDUP = Blueprint('standup', __name__)
 
 
-@STANDUP.route("/standup/start", methods=['POST'])
+@STANDUP.route("/start", methods=['POST'])
 def route_standup_start():
     payload = request.get_json()
     token = payload['token']
     channel_id = payload['channel_id']
-    length = payload['length']
+    length = int(payload['length'])
     return dumps(standup_start(token, channel_id, length))
 
 
-@STANDUP.route("/standup/active", methods=['GET'])
+@STANDUP.route("/active", methods=['GET'])
 def route_standup_active():
-    payload = request.get_json()
-    token = payload['token']
-    channel_id = payload['channel_id']
+    token = request.values.get('token')
+    channel_id = int(request.values.get('channel_id'))
     return dumps(standup_active(token, channel_id))
 
 
-@STANDUP.route("/standup/send", methods=['POST'])
+@STANDUP.route("/send", methods=['POST'])
 def route_standup_send():
     payload = request.get_json()
     token = payload['token']
@@ -42,7 +40,9 @@ def standup_start(token, channel_id, length):
     '''
     Implementation of standup start function.
     '''
-    decode_token(token)
+    token_info = decode_token(token)
+    u_id = token_info['u_id']
+
     channel = helpers.get_channel(channel_id)
 
     if channel is None:
@@ -50,29 +50,42 @@ def standup_start(token, channel_id, length):
 
     if channel['standup']['is_active'] is True:
         raise InputError(
-            description='An active standup is currently running on this channel')
-
-    channel['standup']['is_active'] = True
-
-    timer = threading.Timer(length, stop_standup(channel_id))
-    timer.start()
+            description='An active standup is currently running on this channel'
+        )
 
     finish = int(datetime.now(timezone.utc).timestamp()) + length
 
-    return finish
+    for ch in data_store['channels']:
+        if channel_id == ch['channel_id']:
+            ch['standup']['is_active'] = True
+            ch['standup']['starting_user'] = u_id
+            ch['standup']['time_finish'] = finish
+            break
+
+    timer = threading.Timer(length, stop_standup, args=[token, channel_id])
+    timer.start()
+
+    return {'time_finish': finish}
 
 
-def stop_standup(channel_id):
-    for channel in data_store['channels']:
-        if channel['channel_id'] == channel_id:
-            channel['standup']['is_active'] = False
-            correct_channel = channel
+def stop_standup(token, channel_id):
 
+    ch = helpers.get_channel(channel_id)
     joined_message = ''
 
     # for every message dictionary in messages[]
-    for message in correct_channel['standup']['messages']:
+    for message in ch['standup']['messages']:
         joined_message += f"{message['handle_str']}: {message['message']}\n"
+
+    for channel in data_store['channels']:
+        if channel['channel_id'] == channel_id:
+            channel['standup']['is_active'] = False
+            channel['standup']['starting_user'] = None
+            channel['standup']['time_finish'] = None
+            channel['standup']['messages'].clear()
+            break
+    if len(joined_message) > 0:
+        message_send(token, channel_id, joined_message)
 
 
 def standup_active(token, channel_id):
@@ -104,11 +117,13 @@ def standup_send(token, channel_id, message):
 
     if channel['standup']['is_active'] is False:
         raise InputError(
-            description='An active standup is not currently running in this channel')
+            description=
+            'An active standup is not currently running in this channel')
 
     if u_id not in channel['all_members']:
         raise AccessError(
-            description='The authorised user is not a member of the channel that the message is within'
+            description=
+            'The authorised user is not a member of the channel that the message is within'
         )
 
     message_dict = {'handle_str': helpers.get_handle(u_id), 'message': message}
