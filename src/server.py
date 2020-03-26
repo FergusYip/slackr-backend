2e5c33bd1ef9b7316997510d0b6f2618f76f98d6
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-26 18:22:16 +1100

    removed channels url prefix

--- src/server.py
+++ src/server.py
@@ -38,7 +38,7 @@ APP.register_error_handler(Exception, defaultHandler)
 
 APP.register_blueprint(ADMIN, url_prefix='/admin')
 APP.register_blueprint(AUTH)
-APP.register_blueprint(CHANNELS, url_prefix='/channels')
+APP.register_blueprint(CHANNELS)
 APP.register_blueprint(USER, url_prefix='/user')
 APP.register_blueprint(MESSAGE, url_prefix='/message')
 APP.register_blueprint(CHANNEL, url_prefix='/channel')
