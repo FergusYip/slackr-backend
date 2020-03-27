342d70242b230cfac4e7ad4b9ab0e94cf3d0db38
Cameron Day <z5264584@unsw.edu.au>
2020-03-27 23:46:21 +1100

    Adding http tests for message_remove function.

--- src/http_test/message_test/message_remove_test.py
+++ src/http_test/message_test/message_remove_test.py
@@ -1,84 +1,253 @@
-import channels
-import channel
-import message
-from error import AccessError
-from error import InputError
+'''
+Testing the message_remove functionality.
+'''
+
+import requests
 import pytest
-import auth
+
+BASE_URL = 'http://127.0.0.1:8080'
 
 # =====================================================
-# ========== TESTING MESSAGE REMOVE FUNCTION ==========
+# ========= TESTING MESSAGE REMOVE FUNCTION ===========
 # =====================================================
 
-def test_remove(test_channel, test_user):
+def test_remove_returntype(reset, new_user, new_channel):
+    '''
+    Testing the return type of the message/remove route.
+    '''
 
-    ''' Testing an average case where a user will remove their own message. '''
+    user = new_user()
+    channel = new_channel(user)
 
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
-    message.message_remove(test_user['token'], new_message['message_id'])
+    # Sending the first message in a channel.
+    message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message'
+    }
 
+    test_message = requests.post(f'{BASE_URL}/message/send',
+                                 json=message_info).json().raise_for_status
 
-def test_remove_two(test_channel, test_user):
+    assert test_message['message_id'] == 1
 
-    ''' Case where a default user will remove multiple messages in a row. '''
+    # Sending a second message as to not raise an error if no messages exist once one is deleted.
+    second_message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message2'
+    }
 
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
-    second_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message2')
+    second_test_message = requests.post(f'{BASE_URL}/message/send',
+                                        json=second_message_info).json().raise_for_status
 
-    message.message_remove(test_user['token'], second_message['message_id'])
-    message.message_remove(test_user['token'], new_message['message_id'])
+    assert second_test_message['message_id'] == 2
 
+    # Will now get a list of messages in the channel from index 0.
+    func_input = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0
+    }
 
-def test_remove_wrong_id(test_channel, test_user):
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
 
-    ''' Testing if an InputError is thrown when an invalid message_id is input. '''
+    # Now calling the removal function.
+    remove_input = {
+        'token': user['token'],
+        'message_id': message_from_data[0]['message_id']
+    }
 
-    with pytest.raises(InputError):
-            message.message_remove(test_user['token'], -1)
+    remove_return = requests.delete(f'{BASE_URL}/message/remove', json=remove_input).json()
 
-
-def test_remove_unauthorized(test_channel, test_user, new_user):
-
-    ''' Testing that an AccessError is thrown when a default user is trying to remove
-    another default user's message'''
-
-    second_user = new_user('tester2@gmail.com')
-    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
-
-    with pytest.raises(AccessError):
-        message.message_remove(second_user['token'], new_message['message_id'])
-
-
-def test_remove_owner(test_channel, test_user, new_user):
-
-    ''' Testing that a channel owner has the ability to remove another user's message. '''
-
-    second_user = new_user('tester3@gmail.com')
-    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
-
-    new_message = message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
-    message.message_remove(test_user['token'], new_message['message_id'])
-
-
-def test_remove_new_owners(test_channel, test_user, new_user):
-
-    ''' Testing that added owners have the ability to remove other user's messages inside
-    that channel '''
-
-    second_user = new_user('test2@test.com')
-    channel.channel_invite(test_user['token'], test_channel['channel_id'], second_user['u_id'])
-    channel.channel_addowner(test_user['token'], test_channel['channel_id'], second_user['u_id'])
-
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
-    message.message_remove(second_user['token'], new_message['message_id'])
+    assert isinstance(remove_return, dict)
 
 
-def test_remove_invalidtoken(test_channel, test_user, invalid_token):
+def test_remove_message(reset, new_user, new_channel):
+    '''
+    Testing the return type of the message/remove route.
+    '''
 
-    ''' Testing that an invalid token will raise an AccessError. '''
+    user = new_user()
+    channel = new_channel(user)
 
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
+    # Sending the first message in a channel.
+    message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message'
+    }
 
-    with pytest.raises(AccessError):
-        message.message_remove(invalid_token, new_message['message_id'])
+    test_message = requests.post(f'{BASE_URL}/message/send',
+                                 json=message_info).json().raise_for_status
+
+    assert test_message['message_id'] == 1
+
+    # Sending a second message as to not raise an error if no messages exist once one is deleted.
+    second_message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message2'
+    }
+
+    second_test_message = requests.post(f'{BASE_URL}/message/send',
+                                        json=second_message_info).json().raise_for_status
+
+    assert second_test_message['message_id'] == 2
+
+    # Will now get a list of messages in the channel from index 0.
+    func_input = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0
+    }
+
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
+
+    # Now calling the removal function.
+    remove_input = {
+        'token': user['token'],
+        'message_id': message_from_data[0]['message_id']
+    }
+
+    requests.delete(f'{BASE_URL}/message/remove', json=remove_input).json()
+
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
+
+    # Asser that message with message_id '1' was removed so the 0 index
+    # in the channel is message_id '2'. And that only 1 message exists.
+    assert len(message_from_data['messages']) == 1
+    assert message_from_data[0]['message_id'] == 2
+
+
+def test_remove_invalid_message(reset, new_user, new_channel):
+    '''
+    Testing that an error is raised if the channel or message is invalid.
+    '''
+
+    user = new_user()
+    channel = new_channel(user)
+
+    # Sending the first message in a channel.
+    message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message'
+    }
+
+    test_message = requests.post(f'{BASE_URL}/message/send',
+                                 json=message_info).json().raise_for_status
+
+    assert test_message['message_id'] == 1
+
+    # Getting a list of messages in the channel.
+    func_input = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0
+    }
+
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
+
+    # Removing an invalid message_id. As index 1 does not exist.
+    remove_input = {
+        'token': user['token'],
+        'message_id': message_from_data[1]['message_id']
+    }
+
+    with pytest.raises(requests.HTTPError):
+        requests.delete(f'{BASE_URL}/message/remove', json=remove_input).json()
+
+
+def test_remove_notadmin(reset, new_user, new_channel):
+    '''
+    Testing that a user that is not admin or server owner will not be able
+    to remove messages in the channel, and the function will raise an error.
+    '''
+
+    user = new_user()
+    second_user = new_user(email='tester@test.com')
+
+    channel = new_channel(user)
+
+    # Have second_user join the channel.
+    function_input = {
+        'token': second_user['token'],
+        'channel_id': channel['channel_id']
+    }
+
+    check_dict = requests.post(f'{BASE_URL}/channel/join',
+                               json=function_input).json().raise_for_status
+
+    assert isinstance(check_dict, dict)
+
+    # Have user send a message in the channel.
+    message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message'
+    }
+
+    test_message = requests.post(f'{BASE_URL}/message/send',
+                                 json=message_info).json().raise_for_status
+
+    assert test_message['message_id'] == 1
+
+    # Get a list of all the messages from index 0 in the channel.
+    func_input = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0
+    }
+
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
+
+    # second_user is not admin or server owner.
+    remove_input = {
+        'token': second_user['token'],
+        'message_id': message_from_data[0]['message_id']
+    }
+
+    with pytest.raises(requests.HTTPError):
+        requests.delete(f'{BASE_URL}/message/remove', json=remove_input).json()
+
+
+def test_remove_invalid_token(reset, new_user, new_channel, invalid_token):
+    '''
+    Testing that attempting to remove a message with an invalid token will
+    raise an error.
+    '''
+
+    user = new_user(email='notusedyet@test.com')
+    channel = new_channel(user)
+
+    # Have user send a message in the channel.
+    message_info = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'Message'
+    }
+
+    test_message = requests.post(f'{BASE_URL}/message/send',
+                                 json=message_info).raise_for_status()
+
+    assert test_message['message_id'] == 1
+
+    # Get a list of all the messages from index 0 in the channel.
+    func_input = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0
+    }
+
+    message_from_data = requests.get(f'{BASE_URL}/channel/messages', json=func_input).json()
+
+    # While the message exists at index 0, the token is invalid.
+    remove_input = {
+        'token': invalid_token,
+        'message_id': message_from_data[0]['message_id']
+    }
+
+    with pytest.raises(requests.HTTPError):
+        requests.post(f'{BASE_URL}/message/remove',
+                      json=remove_input).raise_for_status()
