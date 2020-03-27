1a395f8fb968965a0fa7330e03a74d0a799813fc
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 10:54:49 +1100

    Linted auth/logout tests

--- src/http_test/auth_test/auth_logout_http_test.py
+++ src/http_test/auth_test/auth_logout_http_test.py
@@ -1,10 +1,11 @@
+'''Pytest script for testing /auth/logout route'''
 import requests
 import pytest
 
 BASE_URL = 'http://127.0.0.1:8080'
 
 
-def test_logout(reset, new_user):
+def test_logout(reset, new_user):  # pylint: disable=W0613
     '''Test that auth_logout returns True on successful Logout'''
 
     user = new_user()
@@ -13,7 +14,7 @@ def test_logout(reset, new_user):
     assert logout['is_success']
 
 
-def test_logout_invalid_token(reset, invalid_token):
+def test_logout_invalid_token(reset, invalid_token):  # pylint: disable=W0613
     '''Test that auth_logout raises an AccessError when given invalid token'''
 
     logout_input = {'token': invalid_token}
