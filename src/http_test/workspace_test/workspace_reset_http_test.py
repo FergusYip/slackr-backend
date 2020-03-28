50c7b7f7a0580111a995c8979f2f59109e1c4635
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 22:11:44 +1100

    workspace http test 10/10 pylint

--- src/http_test/workspace_test/workspace_reset_http_test.py
+++ src/http_test/workspace_test/workspace_reset_http_test.py
@@ -1,3 +1,4 @@
+'''Pytest script for testing /workspace/reset route'''
 import requests
 import pytest
 
@@ -5,8 +6,9 @@ BASE_URL = 'http://127.0.0.1:8080'
 
 
 def test_workspace_reset_user(reset, new_user):  # pylint: disable=W0613
+    '''Test that the number of users are reset'''
     user_a = new_user(email='user_a@email.com')
-    user_b = new_user(email='user_b@email.com')
+    new_user(email='user_b@email.com')
 
     users_all_input = {'token': user_a['token']}
     all_users = requests.get(f'{BASE_URL}/users/all',
@@ -18,7 +20,7 @@ def test_workspace_reset_user(reset, new_user):  # pylint: disable=W0613
 
     user_c = new_user(email='user_c@email.com')
 
-    users_all_input = {'token': user_a['token']}
+    users_all_input = {'token': user_c['token']}
     all_users = requests.get(f'{BASE_URL}/users/all',
                              params=users_all_input).json()
 
@@ -26,6 +28,7 @@ def test_workspace_reset_user(reset, new_user):  # pylint: disable=W0613
 
 
 def test_workspace_reset_channels(reset, new_user, new_channel):  # pylint: disable=W0613
+    '''Test that the number of channels is reset'''
     user = new_user()
     new_channel(user, 'Channel A')
     new_channel(user, 'Channel B')
@@ -48,6 +51,7 @@ def test_workspace_reset_channels(reset, new_user, new_channel):  # pylint: disa
 
 
 def test_workspace_reset_token_blacklist(reset, new_user):  # pylint: disable=W0613
+    '''Test that the token blacklist is reset'''
     user = new_user()
 
     requests.post(f'{BASE_URL}/workspace/reset')
