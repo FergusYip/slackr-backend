26587b14d65284b728ad4cceea92fddbc2fedc4d
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 12:31:45 +1100

    Tidied auth logout sys test

--- src/tests/auth_test/auth_logout_test.py
+++ src/tests/auth_test/auth_logout_test.py
@@ -1,20 +1,13 @@
+''' System tests for auth_logout'''
 import pytest
 import auth
 from error import InputError, AccessError
 
 
-@pytest.fixture
-def paris():
-    '''Fixture for a creating a user named Paris Cler'''
-
-    return auth.auth_register('pariscler@email.com', 'pariscler0229', 'Paris',
-                              'Cler')
-
-
-def test_logout(paris):
+def test_logout(test_user):
     '''Test that auth_logout returns True on successful Logout'''
 
-    assert auth.auth_logout(paris['token'])['is_success']
+    assert auth.auth_logout(test_user['token'])['is_success']
 
 
 def test_logout_invalid_token(invalid_token):
