09fab224141bc2933f3864109fd03d0c93c60b35
Cameron Day <z5264584@unsw.edu.au>
2020-03-29 10:52:10 +1100

    Updating tests for 100% coverage."

--- src/tests/message_test/message_remove_test.py
+++ src/tests/message_test/message_remove_test.py
@@ -1,9 +1,12 @@
-import channels
+'''
+System testing the functionality of message_remove.
+'''
+
+import pytest
 import channel
 import message
 from error import AccessError
 from error import InputError
-import pytest
 import auth
 
 # =====================================================
@@ -25,7 +28,8 @@ def test_remove_two(reset, test_channel, test_user): # pylint: disable=W0613
     '''
 
     new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
-    second_message = message.message_send(test_user['token'], test_channel['channel_id'], 'Message2')
+    second_message = message.message_send(test_user['token'],
+                                          test_channel['channel_id'], 'Message2')
 
     message.message_remove(test_user['token'], second_message['message_id'])
     message.message_remove(test_user['token'], new_message['message_id'])
@@ -37,7 +41,7 @@ def test_remove_wrong_id(reset, test_channel, test_user): # pylint: disable=W061
     '''
 
     with pytest.raises(InputError):
-            message.message_remove(test_user['token'], -1)
+        message.message_remove(test_user['token'], -1)
 
 
 def test_remove_unauthorized(reset, test_channel, test_user, new_user): # pylint: disable=W0613
@@ -64,24 +68,6 @@ def test_remove_owner(reset, test_channel, test_user, new_user): # pylint: disab
 
     new_message = message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
     message.message_remove(test_user['token'], new_message['message_id'])
-
-
-"""def test_remove_new_owners(reset, make_join_channel, new_user): # pylint: disable=W0613
-    '''
-    Testing that added owners have the ability to remove other user's messages inside
-    that channel.
-    '''
-    
-    first_user = new_user(email='test@test.com')
-    second_user = new_user(email='test2@test.com')
-
-    channel_one = make_join_channel(first_user, 'Channel')
-
-    channel.channel_invite(first_user['token'], channel_one['channel_id'], second_user['u_id'])
-    channel.channel_addowner(first_user['token'], channel_one['channel_id'], second_user['u_id'])
-
-    new_message = message.message_send(first_user['token'], channel_one['channel_id'], 'Message')
-    message.message_remove(second_user['token'], new_message['message_id'])"""
 
 
 def test_remove_invalidtoken(reset, test_channel, test_user): # pylint: disable=W0613
--- src/tests/message_test/message_send_test.py
+++ src/tests/message_test/message_send_test.py
@@ -1,38 +1,51 @@
-import channels
+'''
+System testing the functionality of message_send.
+'''
+
+import pytest
 import channel
 import message
 from error import AccessError
 from error import InputError
-import pytest
-import auth
 
 # =====================================================
 # ========== TESTING MESSAGE SEND FUNCTION ============
 # =====================================================
 
-def test_send(reset, test_channel, test_user):
-
-    ''' Testing an average case where a created user in a channel sends a message '''
+def test_send(reset, test_channel, test_user): # pylint: disable=W0613
+    '''
+    Testing an average case where a created user in a channel sends a message
+    '''
 
     message.message_send(test_user['token'], test_channel['channel_id'], 'Message')
 
 
-def test_send_unauthorized(reset, test_channel, test_user, new_user):
-
-    ''' Testing that an AccessError is thrown when a user attempts to send a
-    message in a channel to which they are not joined. '''
+def test_send_unauthorized(reset, test_channel, test_user, new_user): # pylint: disable=W0613
+    '''
+    Testing that an AccessError is thrown when a user attempts to send a
+    message in a channel to which they are not joined.
+    '''
 
     second_user = new_user('test@test.com')
 
     with pytest.raises(AccessError):
         message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
 
+def test_send_nochannel(reset, test_user): # pylint: disable=W0613
+    '''
+    Testing that attempting to send a message in an invalid channel will
+    raise an error.
+    '''
+    with pytest.raises(InputError):
+        message.message_send(test_user['token'], 1, 'Message')
 
-def test_send_authorization_change(reset, test_channel, test_user, new_user):
 
-    ''' Testing a scenario where a user attempts to send a message in a server they have
+def test_send_authorization_change(reset, test_channel, test_user, new_user): # pylint: disable=W0613
+    '''
+    Testing a scenario where a user attempts to send a message in a server they have
     not joined. This should throw an AccessError. After a channel invitation they should
-    be able to send messages inside the channel. '''
+    be able to send messages inside the channel.
+    '''
 
     second_user = new_user('tester2@test.com')
 
@@ -44,33 +57,37 @@ def test_send_authorization_change(reset, test_channel, test_user, new_user):
     message.message_send(second_user['token'], test_channel['channel_id'], 'Message')
 
 
-def test_send_exceed_char_limit(reset, test_channel, test_user):
-
-    ''' Testing an InputError that should be thrown when the message is greater than
-    1000 characters. '''
+def test_send_exceed_char_limit(reset, test_channel, test_user): # pylint: disable=W0613
+    '''
+    Testing an InputError that should be thrown when the message is greater than
+    1000 characters.
+    '''
 
     with pytest.raises(InputError):
         message.message_send(test_user['token'], test_channel['channel_id'], 'i' * 1001)
 
 
-def test_send_within_char_limit(reset, test_channel, test_user):
+def test_send_within_char_limit(reset, test_channel, test_user): # pylint: disable=W0613
+    '''
+    Testing the maximum length of a message sends correctly.
+    '''
 
-    ''' Testing the maximum length of a message sends correctly. '''
-
-    new_message = message.message_send(test_user['token'], test_channel['channel_id'], 'i' * 1000)
+    message.message_send(test_user['token'], test_channel['channel_id'], 'i' * 1000)
 
 
-def test_send_empty(reset, test_channel, test_user, new_user):
-
-    ''' Testing that a message of zero characters raises an InputError. '''
+def test_send_empty(reset, test_channel, test_user, new_user): # pylint: disable=W0613
+    '''
+    Testing that a message of zero characters raises an InputError.
+    '''
 
     with pytest.raises(InputError):
         message.message_send(test_user['token'], test_channel['channel_id'], '')
 
 
-def test_send_invalidtoken(reset, test_channel, test_user, invalid_token):
-
-    ''' Testing that an invalid token will raise an AccessError. '''
+def test_send_invalidtoken(reset, test_channel, test_user, invalid_token): # pylint: disable=W0613
+    '''
+    Testing that an invalid token will raise an AccessError.
+    '''
 
     with pytest.raises(AccessError):
         message.message_send(invalid_token, test_channel['channel_id'],
