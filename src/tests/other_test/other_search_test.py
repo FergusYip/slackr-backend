6d5f081872f970862e97f5a504979e5a6e724895
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 22:46:55 +1100

    finished system tests

--- src/tests/other_test/other_search_test.py
+++ src/tests/other_test/other_search_test.py
@@ -6,23 +6,23 @@ import other
 from error import AccessError
 
 
-def test_search_no_channel(test_user):
+def test_search_no_channel(reset, test_user):  # pylint: disable=W0613
     '''Test search function when there is no channel'''
 
     assert len(other.search(test_user['token'], '')['messages']) == 0
 
 
-def test_search_empty_channel(test_user, make_join_channel):
+def test_search_empty_channel(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test search function when channel has no messages'''
 
-    test_channel = make_join_channel(test_user, 'Channel')
+    test_channel = new_channel(test_user, 'Channel')
     assert len(other.search(test_user['token'], '')['messages']) == 0
 
 
-def test_search_return_type(test_user, make_join_channel):
+def test_search_return_type(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test the types of values returned by search'''
 
-    test_channel = make_join_channel(test_user, 'Channel')
+    test_channel = new_channel(test_user, 'Channel')
     message.message_send(test_user['token'], test_channel['channel_id'],
                          'Hello world!')
 
@@ -34,10 +34,10 @@ def test_search_return_type(test_user, make_join_channel):
     assert isinstance(results['time_created'], int)
 
 
-def test_search_single_channel(test_user, make_join_channel):
+def test_search_single_channel(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test that the sole message is returned by search'''
 
-    test_channel = make_join_channel(test_user, 'Channel')
+    test_channel = new_channel(test_user, 'Channel')
     message.message_send(test_user['token'], test_channel['channel_id'],
                          'Hello world!')
 
@@ -49,10 +49,10 @@ def test_search_single_channel(test_user, make_join_channel):
     assert msg_in_channel == msg_in_search
 
 
-def test_search_multiple_messages(test_user, make_join_channel):
+def test_search_multiple_messages(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test search with multiple unique messages'''
 
-    test_channel = make_join_channel(test_user, 'Channel')
+    test_channel = new_channel(test_user, 'Channel')
 
     message.message_send(test_user['token'], test_channel['channel_id'],
                          'Alpha')
@@ -66,28 +66,28 @@ def test_search_multiple_messages(test_user, make_join_channel):
     assert len(other.search(test_user['token'], 'Romeo')['messages']) == 0
 
 
-def test_search_multiple_channels(test_user, make_join_channel):
+def test_search_multiple_channels(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test that search works on multiple channels'''
 
-    ch1 = make_join_channel(test_user, 'Channel 1')
+    ch1 = new_channel(test_user, 'Channel 1')
     message.message_send(test_user['token'], ch1['channel_id'], 'Channel 1')
 
-    ch2 = make_join_channel(test_user, 'Channel 2')
+    ch2 = new_channel(test_user, 'Channel 2')
     message.message_send(test_user['token'], ch2['channel_id'], 'Channel 2')
 
-    ch3 = make_join_channel(test_user, 'Channel 3')
+    ch3 = new_channel(test_user, 'Channel 3')
     message.message_send(test_user['token'], ch3['channel_id'], 'Channel 3')
 
     assert len(other.search(test_user['token'], 'Channel')['messages']) == 3
 
 
-def test_search_unauthorised_channels(new_user, make_join_channel):
+def test_search_unauthorised_channels(reset, new_user, new_channel):  # pylint: disable=W0613
     '''Test that users cannot search unauthorised channels'''
 
     tom = new_user('tom@email.com')
 
     jerry = new_user('jerry@email.com')
-    mice_ch = make_join_channel(jerry, 'Mice Only')
+    mice_ch = new_channel(jerry, 'Mice Only')
 
     message.message_send(jerry['token'], mice_ch['channel_id'],
                          'Tom can\'t see this')
@@ -96,10 +96,10 @@ def test_search_unauthorised_channels(new_user, make_join_channel):
     assert len(other.search(tom['token'], '')['messages']) == 0
 
 
-def test_search_case_insensitive(test_user, make_join_channel):
+def test_search_case_insensitive(reset, test_user, new_channel):  # pylint: disable=W0613
     '''Test that query string is not case sensitive'''
 
-    test_channel = make_join_channel(test_user, 'Channel')
+    test_channel = new_channel(test_user, 'Channel')
     message.message_send(test_user['token'], test_channel['channel_id'],
                          'Hello world!')
     msg_in_channel = channel.channel_messages(test_user['token'],
@@ -109,7 +109,7 @@ def test_search_case_insensitive(test_user, make_join_channel):
     assert msg_in_channel == msg_in_search
 
 
-def test_search_invalid_token(invalid_token):
+def test_search_invalid_token(reset, invalid_token):  # pylint: disable=W0613
     '''Test search function with invalid token'''
 
     with pytest.raises(AccessError):
