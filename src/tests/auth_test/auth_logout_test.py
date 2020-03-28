a614578874fdbb12e5063d4e76a87d5711a1ceb5
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 13:17:10 +1100

    added init and removed input error

--- src/tests/auth_test/auth_logout_test.py
+++ src/tests/auth_test/auth_logout_test.py
@@ -1,16 +1,16 @@
 ''' System tests for auth_logout'''
 import pytest
 import auth
-from error import InputError, AccessError
+from error import AccessError
 
 
-def test_logout(reset, test_user):
+def test_logout(reset, test_user):  # pylint: disable=W0613
     '''Test that auth_logout returns True on successful Logout'''
 
     assert auth.auth_logout(test_user['token'])['is_success']
 
 
-def test_logout_invalid_token(reset, invalid_token):
+def test_logout_invalid_token(reset, invalid_token):  # pylint: disable=W0613
     '''Test that auth_logout raises an AccessError when given invalid token'''
 
     with pytest.raises(AccessError):
