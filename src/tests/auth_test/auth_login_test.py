9e080ee208e15cfee6487b2199a628ec9a1f5df6
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-28 13:38:54 +1100

    used conftest instead of paris

--- src/tests/auth_test/auth_login_test.py
+++ src/tests/auth_test/auth_login_test.py
@@ -1,72 +1,78 @@
+''' System tests for auth_login'''
 import pytest
 import auth
 from error import InputError
 
 
-@pytest.fixture
-def paris():
-    '''Fixture for a creating a user named Paris Cler'''
-
-    return auth.auth_register('pariscler@email.com', 'pariscler0229', 'Paris',
-                              'Cler')
-
-
-def test_login_return_type(paris):
+def test_login_return_type(reset, new_user):  # pylint: disable=W0613
     '''Test the types of values returned by auth_login'''
+    new_user(email='hello@world.com', password='pythonIsKool')
+    user_login = auth.auth_login('hello@world.com', 'pythonIsKool')
+    assert isinstance(user_login['u_id'], int)
+    assert isinstance(user_login['token'], str)
 
-    paris_login = auth.auth_login('pariscler@email.com', 'pariscler0229')
-    assert isinstance(paris_login['u_id'], int)
-    assert isinstance(paris_login['token'], str)
 
-
-def test_login_u_id(paris):
+def test_login_u_id(reset, new_user):  # pylint: disable=W0613
     '''Test that the u_id returned auth_login matches u_id returned by auth_register'''
+    user = new_user(email='hello@world.com', password='pythonIsKool')
+    user_login = auth.auth_login('hello@world.com', 'pythonIsKool')
+    assert user_login['u_id'] == user['u_id']
 
-    paris_login = auth.auth_login('pariscler@email.com', 'pariscler0229')
-    assert paris_login['u_id'] == paris['u_id']
 
-
-def test_login_password(paris):
+def test_login_password(reset, new_user):  # pylint: disable=W0613
     '''Test auth_login with an incorrect password'''
+
+    new_user(email='hello@world.com', password='pythonIsKool')
 
     # Password is not correct
     with pytest.raises(InputError):
-        auth.auth_login('pariscler@email.com', 'incorrect_password')
+        auth.auth_login('hello@world.com', 'pythonSucks')
 
 
-def test_login_invalid_user():
+def test_login_invalid_user(reset):  # pylint: disable=W0613
     '''Test with an email which does not belong to a user'''
 
     with pytest.raises(InputError):
         auth.auth_login('non_existent_user@email.com', '12345678')
 
 
-def test_login_multiple_sessions(paris):
+def test_login_multiple_sessions(reset, new_user):  # pylint: disable=W0613
     '''Test that auth_login can create multiple session at once'''
 
-    session_1 = auth.auth_login('pariscler@email.com', 'pariscler0229')
-    session_2 = auth.auth_login('pariscler@email.com', 'pariscler0229')
-    session_3 = auth.auth_login('pariscler@email.com', 'pariscler0229')
-
-    session_tokens = [
-        paris['token'], session_1['token'], session_2['token'],
-        session_3['token']
-    ]
-
-    # Verify that all session_tokens are unique
-    assert len(set(session_tokens)) == len(session_tokens)
+    new_user(email='hello@world.com', password='pythonIsKool')
+    auth.auth_login('hello@world.com', 'pythonIsKool')
+    auth.auth_login('hello@world.com', 'pythonIsKool')
+    auth.auth_login('hello@world.com', 'pythonIsKool')
 
 
-def test_login_email_valid(valid_emails):
+def test_login_unique_token(reset, new_user):
+    '''Test that auth_login tokens are unique to the user'''
+    new_user(email='user_1@email.com', password='password')
+    new_user(email='user_2@email.com', password='password')
+
+    user_1_login = auth.auth_login('user_1@email.com', 'password')
+    user_2_login = auth.auth_login('user_2@email.com', 'password')
+
+    tokens = [user_1_login['token'], user_2_login['token']]
+
+    # Verify that all tokens are unique
+    assert len(set(tokens)) == len(tokens)
+
+
+def test_login_email_valid(reset, valid_emails, new_user):  # pylint: disable=W0613
     '''Test input of valid emails into auth_login'''
 
     for email in valid_emails:
+        new_user(email=email)  # Email must belong to a user
         auth.auth_login(email, 'password')
 
 
-def test_login_email_invalid(invalid_emails):
+def test_login_email_invalid(reset, invalid_emails):  # pylint: disable=W0613
     '''Test input of invalid emails into auth_login'''
 
     for email in invalid_emails:
+        # No user was created here.
+        # Assume that email checking occurs before checking if email belongs to user
+
         with pytest.raises(InputError):
             auth.auth_login(email, 'password')
