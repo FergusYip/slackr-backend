cb63d4e62643e8b0c864ccdeb3d3720d205df6c5
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-29 13:33:01 +1100

    Fixed new_user error

--- src/http_test/channel_test/channel_join_http_test.py
+++ src/http_test/channel_test/channel_join_http_test.py
@@ -13,20 +13,17 @@ def test_join_new(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel join function for a public channel.
     '''
 
-    user1 = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
 
     channel = new_channel(user1)
 
-    input_dict = {
-        'token': user2['token'],
-        'channel_id': channel['channel_id']
-    }
+    input_dict = {'token': user2['token'], 'channel_id': channel['channel_id']}
 
     requests.post(f'{BASE_URL}/channel/join', json=input_dict)
 
-    details = requests.get(
-        f'{BASE_URL}/channel/details', params=input_dict).json()
+    details = requests.get(f'{BASE_URL}/channel/details',
+                           params=input_dict).json()
 
     assert len(details['all_members']) == 2
 
@@ -36,15 +33,12 @@ def test_join_id(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel join function for an invalid channel_id
     '''
 
-    user1 = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
 
     new_channel(user1)
 
-    input_dict = {
-        'token': user2['token'],
-        'channel_id': -1
-    }
+    input_dict = {'token': user2['token'], 'channel_id': -1}
 
     with pytest.raises(requests.HTTPError):
         requests.post(f'{BASE_URL}/channel/join',
@@ -56,14 +50,11 @@ def test_join_private(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel_join function for a private channel.
     '''
 
-    user1 = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
     channel = new_channel(user1, is_public=False)
 
-    input_dict = {
-        'token': user2['token'],
-        'channel_id': channel['channel_id']
-    }
+    input_dict = {'token': user2['token'], 'channel_id': channel['channel_id']}
 
     with pytest.raises(requests.HTTPError):
         requests.post(f'{BASE_URL}/channel/join',
@@ -78,15 +69,12 @@ def test_join_member(reset, new_user, new_channel):  # pylint: disable=W0613
     user1 = new_user()
     channel = new_channel(user1)
 
-    input_dict = {
-        'token': user1['token'],
-        'channel_id': channel['channel_id']
-    }
+    input_dict = {'token': user1['token'], 'channel_id': channel['channel_id']}
 
     # This line shouldn't do anything (according to assumption).
     requests.post(f'{BASE_URL}/channel/join', json=input_dict)
 
-    details = requests.get(
-        f'{BASE_URL}/channel/details', params=input_dict).json()
+    details = requests.get(f'{BASE_URL}/channel/details',
+                           params=input_dict).json()
 
     assert len(details['all_members']) == 1
