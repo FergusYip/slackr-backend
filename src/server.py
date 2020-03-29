e06d13084db8dee3208a5f9fb60f0c90f5f8e706
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-29 11:08:29 +1100

    tidied server.py

--- src/server.py
+++ src/server.py
@@ -1,18 +1,18 @@
+'''Backend server file for slackr'''
 import sys
-import pickle
 from json import dumps
 from flask import Flask
 from flask_cors import CORS
 from admin import ADMIN
 from auth import AUTH
-from message import MESSAGE
-from user import USER
-from channels import CHANNELS
 from channel import CHANNEL
+from channels import CHANNELS
+from message import MESSAGE
 from other import OTHER
-from workspace import WORKSPACE
 from standup import STANDUP
-from data_store import data_store, autosave
+from user import USER
+from workspace import WORKSPACE
+from data_store import autosave
 
 AUTOSAVE_ENABLED = True
 DEBUG_MODE = not AUTOSAVE_ENABLED  # Do not change this line
@@ -45,54 +45,6 @@ APP.register_blueprint(CHANNEL)
 APP.register_blueprint(STANDUP, url_prefix='/standup')
 APP.register_blueprint(OTHER)
 APP.register_blueprint(WORKSPACE)
-
-
-@APP.route('/save', methods=['POST'])
-def save_state():
-    with open('data_store.p', 'wb') as FILE:
-        pickle.dump(data_store, FILE)
-    return dumps({})
-
-
-@APP.route('/data', methods=['GET'])
-def data():
-    return dumps(data_store)
-
-
-'''@APP.route("/user/profile", methods=['GET'])
-def user_profile():
-    pass
-
-
-@APP.route("/user/profile/setname", methods=['PUT'])
-def user_profile_setname():
-    pass
-
-
-@APP.route("/user/profile/setemail", methods=['PUT'])
-def user_profile_setemail():
-    pass
-
-
-@APP.route("/user/profile/sethandle", methods=['PUT'])
-def user_profile_handle():
-    pass'''
-
-
-@APP.route("/standup/start", methods=['POST'])
-def standup_start():
-    pass
-
-
-@APP.route("/standup/active", methods=['GET'])
-def standup_active():
-    pass
-
-
-@APP.route("/standup/send", methods=['POST'])
-def standup_send():
-    pass
-
 
 if __name__ == "__main__":
     if AUTOSAVE_ENABLED:
