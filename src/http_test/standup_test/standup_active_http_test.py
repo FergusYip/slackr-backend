e416b12fab229e824ee5a0d09ad83c3b254bf9d8
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-03-29 17:07:02 +1100

    added http tests for standup active

---
+++ src/http_test/standup_test/standup_active_http_test.py
@@ -0,0 +1,69 @@
+'''
+HTTP Tests for the standup_active function.
+'''
+
+from time import sleep
+import requests
+import pytest
+
+BASE_URL = 'http://127.0.0.1:8080'
+
+
+def test_invalid_channel(reset, new_user, new_channel):  # pylint: disable=W0613
+    '''
+    Testing standup active for an invalid channel.
+    '''
+
+    user = new_user()
+    new_channel(user)
+
+    active_in = {
+        'token': user['token'],
+        'channel_id': -1
+    }
+
+    with pytest.raises(requests.HTTPError):
+        requests.get(f'{BASE_URL}/standup/active',
+                     params=active_in).raise_for_status()
+
+
+def test_return(reset, new_user, new_channel):  # pylint: disable=W0613
+    '''
+    Testing return of standup active.
+    '''
+
+    user = new_user()
+    channel = new_channel(user)
+
+    start_in = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'length': 1
+    }
+
+    # starting a standup.
+    requests.post(f'{BASE_URL}/standup/start', json=start_in).json()
+
+    active_in = {
+        'token': user['token'],
+        'channel_id': channel['channel_id']
+    }
+
+    active_out = requests.get(
+        f'{BASE_URL}/standup/active', params=active_in).raise_for_status()
+
+    assert active_out['is_active'] is True
+
+    sleep(1.1)
+
+    assert active_out['is_active'] is False
+
+
+def test_insufficient_params(reset):  # pylint: disable=W0613
+    '''
+    Testing insufficient parameters for standup active
+    '''
+
+    with pytest.raises(requests.HTTPError):
+        requests.get(f"{BASE_URL}/standup/active",
+                     params={}).raise_for_status()
