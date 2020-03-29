bf4304f160636d5f2b44313ada1c91261d53b4af
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-03-29 13:34:33 +1100

    Fixed new_user error

--- src/http_test/channel_test/channel_leave_http_test.py
+++ src/http_test/channel_test/channel_leave_http_test.py
@@ -13,32 +13,26 @@ def test_leave_new(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel leave function for a public channel.
     '''
 
-    user1 = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
 
     channel = new_channel(user1)
 
-    input_dict = {
-        'token': user2['token'],
-        'channel_id': channel['channel_id']
-    }
+    input_dict = {'token': user2['token'], 'channel_id': channel['channel_id']}
 
     requests.post(f'{BASE_URL}/channel/join', json=input_dict)
 
-    details = requests.get(
-        f'{BASE_URL}/channel/details', params=input_dict).json()
+    details = requests.get(f'{BASE_URL}/channel/details',
+                           params=input_dict).json()
 
     assert len(details['all_members']) == 2
 
     requests.post(f'{BASE_URL}/channel/leave', json=input_dict)
 
-    details_in = {
-        'token': user1['token'],
-        'channel_id': channel['channel_id']
-    }
+    details_in = {'token': user1['token'], 'channel_id': channel['channel_id']}
 
-    details = requests.get(
-        f'{BASE_URL}/channel/details', params=details_in).json()
+    details = requests.get(f'{BASE_URL}/channel/details',
+                           params=details_in).json()
 
     assert len(details['all_members']) == 1
 
@@ -52,10 +46,7 @@ def test_leave_ch(reset, new_user, new_channel):  # pylint: disable=W0613
 
     new_channel(user)
 
-    input_dict = {
-        'token': user['token'],
-        'channel_id': -1
-    }
+    input_dict = {'token': user['token'], 'channel_id': -1}
 
     with pytest.raises(requests.HTTPError):
         requests.post(f'{BASE_URL}/channel/leave',
@@ -67,30 +58,24 @@ def test_leave_owner(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel leave for owner member.
     '''
 
-    user = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
 
-    channel = new_channel(user)
+    channel = new_channel(user1)
 
-    join_dict = {
-        'token': user2['token'],
-        'channel_id': channel['channel_id']
-    }
+    join_dict = {'token': user2['token'], 'channel_id': channel['channel_id']}
 
     # user2 joins the channel.
     requests.post(f'{BASE_URL}/channel/join', json=join_dict)
 
-    leave_dict = {
-        'token': user['token'],
-        'channel_id': channel['channel_id']
-    }
+    leave_dict = {'token': user1['token'], 'channel_id': channel['channel_id']}
 
     # user leaves the channel.
     requests.post(f'{BASE_URL}/channel/leave', json=leave_dict)
 
     # user2 gets details of channel.
-    details = requests.get(
-        f'{BASE_URL}/channel/details', params=join_dict).json()
+    details = requests.get(f'{BASE_URL}/channel/details',
+                           params=join_dict).json()
 
     assert len(details['owner_members']) == 0
 
@@ -100,15 +85,12 @@ def test_leave_member(reset, new_user, new_channel):  # pylint: disable=W0613
     Testing channel leave for non-member.
     '''
 
-    user = new_user()
-    user2 = new_user()
+    user1 = new_user(email='user_1@email.com')
+    user2 = new_user(email='user_2@email.com')
 
-    channel = new_channel(user)
+    channel = new_channel(user1)
 
-    input_dict = {
-        'token': user2['token'],
-        'channel_id': channel['channel_id']
-    }
+    input_dict = {'token': user2['token'], 'channel_id': channel['channel_id']}
 
     with pytest.raises(requests.HTTPError):
         requests.post(f'{BASE_URL}/channel/leave',
