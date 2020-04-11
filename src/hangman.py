e83de9b7920dd21798d7e5f671faddf8a421b9bf
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-04-11 12:53:23 +1000

    made pylint compliant and simplified code.

--- src/hangman.py
+++ src/hangman.py
@@ -1,15 +1,23 @@
+'''
+Implementation of hangman game in a channel.
+'''
+
 from string import ascii_lowercase
+import random
+from PyLyrics import PyLyrics
 from data_store import DATA_STORE as data_store
 import helpers
 from token_validation import decode_token
-from error import AccessError, InputError
-import PyLyrics
-import random
+from error import InputError
 import message
 import token_validation
 
 
 def start_hangman(token, channel_id):
+    '''
+    Initializes the hangman game.
+    '''
+
     bot_token = token_validation.encode_token(-95)
     data_store['hangman_bot']['token'] = bot_token
 
@@ -19,17 +27,20 @@ def start_hangman(token, channel_id):
         raise InputError(description='Channel does not exist.')
 
     # setting hangman is_active to true.
-    for ch in data_store['channels']:
-        if channel_id == ch['channel_id']:
-            ch['hangman']['is_active'] = True
-            ch['hangman']['word'] = getLine().lower()
-
-    dashes = '_' * len(ch['hangman']['word'])
+    for channel in data_store['channels']:
+        if channel_id == channel['channel_id']:
+            channel['hangman']['is_active'] = True
+            channel['hangman']['word'] = get_line().lower()
+            dashes = '_' * len(channel['hangman']['word'])
 
     message.message_send(bot_token, channel_id, dashes)
 
 
 def guess_hangman(token, channel_id, guess):
+    '''
+    Main logic for hangman game.
+    '''
+
     decode_token(token)
     channel = helpers.get_channel(channel_id)
 
@@ -50,51 +61,50 @@ def guess_hangman(token, channel_id, guess):
     if channel is None:
         raise InputError(description='Channel does not exist.')
 
-    for ch in data_store['channels']:
-        if channel_id == ch['channel_id']:
+    for channel in data_store['channels']:
+        if channel_id == channel['channel_id']:
 
             # checking if hangman is_active is true.
-            if ch['hangman']['is_active'] is False:
+            if channel['hangman']['is_active'] is False:
                 raise InputError(description='game not active')
 
             # appending to list of guesses.
-            ch['hangman']['guesses'].append(guess)
+            channel['hangman']['guesses'].append(guess)
+
+            # printing dashes.
+            dashed = get_dashed(
+                channel['hangman']['word'], channel['hangman']['guesses'])
+            message.message_send(token, channel_id, dashed)
 
             # if more than 10 guesses.
-            if len(ch['hangman']['guesses']) > 10:
+            if len(channel['hangman']['guesses']) > 10:
                 message.message_send(token, channel_id, 'Game Over')
-                ch['hangman']['is_active'] = False
+                channel['hangman']['is_active'] = False
 
             # incorrect guess.
-            elif guess not in ch['hangman']['word']:
+            elif guess not in channel['hangman']['word']:
                 # printing the stage of game.
-                wrongGuesses = len(ch['hangman']['guesses']) - \
-                    len(ch['hangman']['correct'])
+                wrong_guesses = len(channel['hangman']['guesses']) - \
+                    len(channel['hangman']['correct'])
                 message.message_send(
-                    token, channel_id, stages[wrongGuesses])
-
-                # printing the dashes.
-                message.message_send(
-                    token, channel_id, getDashed(ch['hangman']['word'], ch['hangman']['guesses']))
+                    token, channel_id, stages[wrong_guesses])
 
             # correct guess.
             else:
-                ch['hangman']['correct'].append(guess)
-
-                # printing the dashes.
-                message.message_send(
-                    token, channel_id, getDashed(ch['hangman']['word'], ch['hangman']['guesses']))
+                channel['hangman']['correct'].append(guess)
 
     # full word is guessed.
     if '_' not in dashed and len(channel['hangman']['guesses']) <= 10:
         message.message_send(token, channel_id, dashed)
         message.message_send(token, channel_id, 'Congratulations!')
-        return
-
-    message.message_send(token, channel_id, stages[stage_var])
 
 
-def getDashed(word, guesses):
+def get_dashed(word, guesses):
+    '''
+    Returns a string where all unguessed letters are '_'.
+    '''
+
+    # getting list of all lowercase letters.
     lowercase = [char for char in ascii_lowercase]
     dashed = word
     for char in lowercase:
@@ -103,12 +113,13 @@ def getDashed(word, guesses):
     return dashed
 
 
-def getLine():
+def get_line():
     '''
-    Function to get random line with PyLyrics
+    Function to get random line with PyLyrics.
     '''
 
-    lyrics = PyLyrics.getLyrics('Rick Astley', 'Never Gonna Give You Up')
+    lyrics = PyLyrics.getLyrics(
+        'Rick Astley', 'Never Gonna Give You Up')
 
     lyrics = lyrics.split('\n')
 
