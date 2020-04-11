6f070013e5e898c31affbcdaa1cd147eb204443f
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-04-11 14:13:21 +1000

    Added sys test for password reset request

---
+++ src/tests/auth_test/auth_passwordreset_request_test.py
@@ -0,0 +1,15 @@
+''' System tests for auth_passwordreset_request'''
+import time
+import auth
+from .read_email_helper import get_num_emails_from_chunts, delete_all_emails
+
+
+def test_reset_request_sent_email(reset, new_user):  # pylint: disable=W0613
+    '''Test that an email is sent to the user when a password reset is requested'''
+    email = 'thechunts.slackr@gmail.com'
+    new_user(email=email, password='pythonIsKool')
+    delete_all_emails()
+    auth.auth_passwordreset_request(email)
+    time.sleep(10)
+    assert get_num_emails_from_chunts() == 1
+    delete_all_emails()
