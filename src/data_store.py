47c6d148330f083703833d7a5759a2b1544f2e97
Wai Lam Fergus Yip <z5248282@unsw.edu.au>
2020-04-13 17:28:55 +1000

    Added missing comma

--- src/data_store.py
+++ src/data_store.py
@@ -36,7 +36,7 @@ except FileNotFoundError:
             'email': 'hangmanBot',
             'name_first': 'Hangman',
             'name_last': 'Bot',
-            'handle_str': 'Hangman Bot'
+            'handle_str': 'Hangman Bot',
             'profile_img_url': 'https://i.imgur.com/olQfW6w.jpg'
         },
         'reset_requests': []
