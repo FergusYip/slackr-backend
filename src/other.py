37b9660b09c9c3215d1c650f047a76428faf5697
Fergus Yip <wailamfergus.yip@student.unsw.edu.au>
2020-04-17 10:34:00 +1000

    pylint

--- src/other.py
+++ src/other.py
@@ -17,19 +17,6 @@ def users_all(token):
 	'''
     decode_token(token)
     users = DATA_STORE.users_all
-    """
-    users = []
-    for user in data_store['users']:
-        user_dict = {
-            'u_id': user['u_id'],
-            'email': user['email'],
-            'name_first': user['name_first'],
-            'name_last': user['name_last'],
-            'handle_str': user['handle_str'],
-            'profile_img_url': user['profile_img_url']
-        }
-        users.append(user_dict)
-    """
     return {'users': users}
 
 
@@ -51,33 +38,6 @@ def search(token, query_str):
         message.details(user) for message in user.viewable_messages
         if query_str.lower() in message.message.lower()
     ]
-    """
-    messages = []
-    for channel in user_channels(token_payload['u_id']):
-        search_results = channel_search(channel, query_str)
-        for message in reversed(search_results):
-
-            message_reacts = []
-            reacts = message['reacts']
-            for react in reacts:
-                is_this_user_reacted = token_payload['u_id'] in react['u_ids']
-                react_info = {
-                    'react_id': react['react_id'],
-                    'u_ids': react['u_ids'],
-                    'is_this_user_reacted': is_this_user_reacted
-                }
-                message_reacts.append(react_info)
-
-            message_info = {
-                'message_id': message['message_id'],
-                'u_id': message['u_id'],
-                'message': message['message'],
-                'time_created': message['time_created'],
-                'reacts': message_reacts,
-                'is_pinned': message['is_pinned']
-            }
-            messages.append(message_info)
-    """
     return {'messages': messages}
 
 
